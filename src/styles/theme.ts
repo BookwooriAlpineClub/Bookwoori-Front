3cd9d38e8107a9b442935318cae2ad5e4a9f53db
Hyerin Sung <hyerinx@naver.com>
2024-10-14 23:39:04 +0900

    🎉 프로젝트 생성 (#1)

---
+++ src/styles/theme.ts
@@ -0,0 +1,23 @@
+const colors = {
+  primary: '#3498db',
+  secondary: '#2ecc71',
+};
+
+const typography = {
+  h1: `
+    font-size: 60px;
+    font-weight: 600;
+  `,
+  h2: `
+    font-size: 50px;
+    font-weight: 500;
+  `,
+  b1: `
+    font-size: 18px;
+    font-weight: 400;
+  `,
+};
+
+const theme = { colors, typography };
+
+export default theme;
