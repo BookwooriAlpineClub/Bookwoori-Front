1d76a81425c3f5d5c9607ada5d3d7a1533ce2fa1
Hyerin Sung <hyerinx@naver.com>
2024-10-17 16:38:38 +0900

    🔧 index.tsx와 App.tsx 병합 (#1)

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,24 @@
+import './styles/reset.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/reset.css';
-import './styles/global.css';
-import App from './App';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RecoilRoot } from 'recoil';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './styles/theme';
+import GlobalStyle from './styles/global';
+import Router from './Router';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement,
-);
-root.render(
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <Router />
+        </ThemeProvider>
+      </RecoilRoot>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
