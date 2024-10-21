955ebeb47d320cacd8b1d1000ee2892b3d9c6ab5
Hyerin Sung <hyerinx@naver.com>
2024-10-21 20:04:39 +0900

    💄 Toast 컴포넌트 퍼블리싱 (#6)

---
+++ src/components/common/Toast.tsx
@@ -0,0 +1,57 @@
+// Toast 컴포넌트 사용법
+// 1. useToast 훅을 사용하세요.
+
+import styled from 'styled-components';
+import { createPortal } from 'react-dom';
+import { useRecoilValue } from 'recoil';
+import { toastState } from '../../states/atoms';
+
+const Toast = () => {
+  const toasts = useRecoilValue(toastState);
+
+  return createPortal(
+    <List>
+      {toasts &&
+        toasts.map((item) => <Item key={item.id}>{item.content}</Item>)}
+    </List>,
+    document.getElementById('toast') as HTMLElement,
+  );
+};
+
+export default Toast;
+
+const List = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  z-index: 900; // 최상위 레이어
+  transform: translate(-50%, -50%);
+`;
+
+const Item = styled.div`
+  min-width: 260px;
+  padding: 4px 15px;
+
+  border-radius: 4px;
+  background-color: rgba(0, 0, 0, 0.7);
+
+  color: white;
+  text-align: center;
+  white-space: pre-line; // 개행문자 처리
+
+  @keyframes fadeOut {
+    from {
+      opacity: 1;
+    }
+    to {
+      opacity: 0;
+    }
+  }
+  animation: fadeOut 2.5s ease 2s forwards;
+
+  // 드래그 및 선택 방지
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-use-select: none;
+  user-select: none;
+`;
