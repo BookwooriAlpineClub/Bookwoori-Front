814165e27ee8f3759399a5073bb8f0666dc7320b
Hyerin Sung <hyerinx@naver.com>
2024-10-23 06:55:43 +0900

    💄 fonts 디자인 토큰 추가 (#9)

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,12 +1,22 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'HS여름물빛체2.0';
+    src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/2405@1.0/HSYeoleum20-Regular.woff2') format('woff2');
+  }
+  @font-face {
+    font-family: 'MaruBuri';
+    src: url(https://hangeul.pstatic.net/hangeul_static/webfont/MaruBuri/MaruBuri-Regular.eot);
+    src: url(https://hangeul.pstatic.net/hangeul_static/webfont/MaruBuri/MaruBuri-Regular.eot?#iefix) format("embedded-opentype"), url(https://hangeul.pstatic.net/hangeul_static/webfont/MaruBuri/MaruBuri-Regular.woff2) format("woff2"), url(https://hangeul.pstatic.net/hangeul_static/webfont/MaruBuri/MaruBuri-Regular.woff) format("woff"), url(https://hangeul.pstatic.net/hangeul_static/webfont/MaruBuri/MaruBuri-Regular.ttf) format("truetype");
+  }
+
   * {
-    font-family: Pretendard;
+    font-family: 'MaruBuri';
     box-sizing: border-box;
   }
   :root {
-    font-family: Pretendard;
+    font-family: 'MaruBuri';
     background-color: white;
   }
   #root {
