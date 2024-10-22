dd4aba3fbf8b154548787546816eff062430e07d
Hyerin Sung <hyerinx@naver.com>
2024-10-22 20:50:10 +0900

    ♻️ Scrim 컴포넌트 분리 (#7)

--- src/components/common/Dialog.tsx
+++ src/components/common/Dialog.tsx
@@ -1,72 +1,65 @@
 /*
 Dialog 컴포넌트 사용법
 
-1. 열기/닫기 제어
-1-1. 부모 컴포넌트에 추가: const [isDialogShow, setIsDialogShow] = useState<boolean>(false);
-1-2. 부모 컴포넌트에 추가: {isDialogShow && (<Dialog></Dialog>)}
+1. 마운트/언마운트 제어
+1-1. 부모 컴포넌트에 <Dialog></Dialog>를 추가한다.
+1-2. 부모 컴포넌트에 state를 추가한다.
+const [isDialogShow, setIsDialogShow] = useState<boolean>(false);
+1-3. 부모 컴포넌트에서 setIsDialogShow로 제어한다.
 
 2. props
-2-1. setIsDialogShow={1-1의 setIsDialogShow}
-예시. <Dialog setIsDialogShow={setIsDialogShow}></Dialog>
+<Dialog
+  isDialogShow={isDialogShow} // 1-2의 isDialogShow
+  setIsDialogShow={setIsDialogShow} // 1-2의 setIsDialogShow
+>
 
 3. 내용
-3-1. 부모 컴포넌트에서 <Dialog></Dialog> 안에 작성합니다.
-예시.
+3-1. 부모 컴포넌트에서 <Dialog></Dialog> 안에 작성한다.
 <Dialog>
   <p>다이얼로그 모달</p>
   <button type='button' onClick={() => setIsDialogShow(false)}>닫기</button>
 </Dialog>
 */
 
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import { useEffect } from 'react';
-import { createPortal } from 'react-dom';
+import Scrim from './Scrim';
 
 interface Props {
+  isDialogShow: boolean;
   setIsDialogShow(isDialogShow: boolean): void;
   children: React.ReactNode;
 }
 
-const Dialog = ({ setIsDialogShow, children }: Props) => {
-  useEffect(() => {
-    // 모달 마운트 시 스크롤 방지
-    document.body.style.cssText = `
-      position: fixed;
-      top: -${window.scrollY}px;
-      width: 100%;
-    `;
-    return () => {
-      // 모달 언마운트 시 이전 상태로 복원
-      const scrollY = document.body.style.top;
-      document.body.style.cssText = '';
-      window.scrollTo(0, parseInt(scrollY || '0', 10) * -1);
-    };
-  }, []);
+const Dialog = ({ isDialogShow, setIsDialogShow, children }: Props) => {
+  const [isMount, setIsMount] = useState<boolean>(false);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
-  return createPortal(
-    <Background onClick={() => setIsDialogShow(false)}>
+  useEffect(() => {
+    if (isDialogShow) {
+      setIsMount(true);
+      setTimeout(() => setIsOpen(true), 30);
+    } else {
+      setIsOpen(false);
+      setTimeout(() => setIsMount(false), 300);
+    }
+  }, [isDialogShow]);
+
+  return (
+    <Scrim isOpen={isOpen} isMount={isMount} setIsModalShow={setIsDialogShow}>
       <Layout onClick={(event) => event.stopPropagation()}>{children}</Layout>
-    </Background>,
-    document.getElementById('modal') as HTMLElement,
+    </Scrim>
   );
 };
 
 export default Dialog;
 
-const Background = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: 800; // Toast 컴포넌트보다 낮은 레벨의 레이어
-
-  width: 100svw;
-  height: 100svh;
-
-  background-color: rgba(15, 16, 21, 0.4); // 추후 수정: theme.colors
-`;
 const Layout = styled.section`
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+
+
   // 추후 수정: 디자인 작업 중
 `;
