4a6563883a2a55208110b48fa92a70ba39df39e4
rwaeng <rwaeng0421@gmail.com>
2024-10-24 20:54:10 +0900

    🔥 불필요한 설정 삭제

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
-  },
-  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
   testEnvironment: 'jsdom',
   moduleNameMapper: {
     '\\.svg$': '<rootDir>/src/tests/__mocks__/svg.js', // SVG 파일에 대한 모킹 처리
