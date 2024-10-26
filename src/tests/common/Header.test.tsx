1dcfec15bea89524f5d9a363e0526013771151ae
rwaeng <rwaeng0421@gmail.com>
2024-10-26 13:39:03 +0900

    💄 Header 컴포넌트 theme 적용 방법 변경에 따른 test 코드 수정

--- src/tests/common/Header.test.tsx
+++ src/tests/common/Header.test.tsx
@@ -2,19 +2,23 @@ import '@testing-library/jest-dom';
 import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
 import Header from '@src/components/common/Header';
+import { ThemeProvider } from 'styled-components';
+import { theme } from '@src/styles/theme';
 
 describe('헤더(공통) 컴포넌트 단위 테스트', () => {
   test('props로 헤더 text를 전달하면 text가 렌더되어야 한다.', () => {
     const headerText = '헤더 제목';
     render(
-      <MemoryRouter>
-        <Routes>
-          <Route
-            path='/'
-            element={<Header text='헤더 제목' headerType='hamburger' />}
-          />
-        </Routes>
-      </MemoryRouter>,
+      <ThemeProvider theme={theme}>
+        <MemoryRouter>
+          <Routes>
+            <Route
+              path='/'
+              element={<Header text='헤더 제목' headerType='hamburger' />}
+            />
+          </Routes>
+        </MemoryRouter>
+      </ThemeProvider>,
     );
 
     const expectText = screen.getByText(headerText);
