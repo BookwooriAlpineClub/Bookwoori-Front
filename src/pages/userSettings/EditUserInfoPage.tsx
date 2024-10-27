d08fadfc53ed2d48facf156974fd18c75b9c8563
rwaeng <rwaeng0421@gmail.com>
2024-10-27 16:02:56 +0900

    ✨ 별명 입력 시 글자 수 변경, 버튼 활성화 여부 변경, 별명 변경 감지 시 글자색 변경 (#30)

--- src/pages/userSettings/EditUserInfoPage.tsx
+++ src/pages/userSettings/EditUserInfoPage.tsx
@@ -1,8 +1,18 @@
 import styled from 'styled-components';
+import { useRef, useState } from 'react';
 import UserProfilImg from '@src/components/userSettings/UserProfileImg';
 import Button from '@src/components/common/Button';
 
 const EditUserInfoPage = () => {
+  const [value, setValue] = useState<string>('내별명');
+  const [length, setLength] = useState<number>(0);
+  const ref = useRef(value);
+
+  const handleChangeValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+    setLength(e.target.value.length);
+  };
+
   return (
     <SLayout>
       <SContainer>
@@ -10,12 +20,18 @@ const EditUserInfoPage = () => {
         <SBox>
           <SLabel>별명</SLabel>
           <SWrapper>
-            <SInput type='text' placeholder='별명을 입력하세요.' />
-            <SSmallLabel>4/10</SSmallLabel>
+            <SInput
+              type='text'
+              placeholder='별명을 입력하세요.'
+              value={value}
+              onChange={handleChangeValue}
+              $color={ref.current === value}
+            />
+            <SSmallLabel>{length}/10</SSmallLabel>
           </SWrapper>
         </SBox>
       </SContainer>
-      <Button>수정하기</Button>
+      <Button disabled={!(value && length <= 10)}>수정하기</Button>
     </SLayout>
   );
 };
@@ -56,14 +72,15 @@ const SWrapper = styled.div`
   padding: 0.875rem 0.625rem;
 `;
 
-const SInput = styled.input`
+const SInput = styled.input<{ $color: boolean }>`
   width: 100%;
 
   border-radius: 0.3125rem;
   background: ${({ theme }) => theme.colors.white};
 
   ${({ theme }) => theme.fonts.body};
-  color: ${({ theme }) => theme.colors.black100};
+  color: ${({ theme, $color }) =>
+    $color ? theme.colors.black100 : theme.colors.blue100};
 `;
 
 const SSmallLabel = styled.label`
