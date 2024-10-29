690e5a55246651b23d7772734d392b8241297e41
Hyerin Sung <hyerinx@naver.com>
2024-10-30 03:38:54 +0900

    🔧 jest 전역 import 설정 (#32)

--- craco.config.ts
+++ craco.config.ts
@@ -1,10 +1,23 @@
-import type { CracoConfig } from '@craco/types';
+import { type CracoConfig } from '@craco/types';
+import { createDefaultPreset } from 'ts-jest';
 import path from 'path';
+
+const defaultPreset = createDefaultPreset();
 
 const cracoConfig: CracoConfig = {
   webpack: {
     alias: {
       '@src': path.resolve(__dirname, 'src'),
+    },
+  },
+  jest: {
+    configure: {
+      testEnvironment: 'jsdom', // web app
+      setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+      moduleNameMapper: {
+        '^@src/(.*)$': '<rootDir>/src/$1',
+      },
+      ...defaultPreset,
     },
   },
 };
