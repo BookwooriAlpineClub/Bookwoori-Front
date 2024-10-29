91ec86e9a9bf124a6064162a38cbf05ecddc585c
rwaeng <rwaeng0421@gmail.com>
2024-10-30 01:13:50 +0900

    💄 채널 리스트 페이지 데이터 수정 (#18)

--- src/pages/channel/ChannelListPage.tsx
+++ src/pages/channel/ChannelListPage.tsx
@@ -1,6 +1,6 @@
 import SubButton from '@src/components/channel/SubButton';
 import Header from '@src/components/common/Header';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { ReactComponent as Edit } from '@src/assets/icons/edit.svg';
 import { ReactComponent as CategoryAdd } from '@src/assets/icons/category_add.svg';
 import { ReactComponent as ChannelAdd } from '@src/assets/icons/channel_add.svg';
@@ -8,18 +8,61 @@ import Accordion from '@src/components/channel/Accordion';
 import Carousel from '@src/components/channel/Carousel';
 import ChannelList from '@src/components/channel/ChannelList';
 
+interface ButtonData {
+  icon: React.ReactNode;
+  label: string;
+}
+
+interface ChannelData {
+  name: string;
+  children: React.ReactNode;
+}
+
+interface ClimbingChannel {
+  climbingId: number;
+  name: string;
+  type: 'text' | 'voice' | 'run';
+}
+
+const mockList: ClimbingChannel[] = [
+  {
+    climbingId: 7,
+    name: '클라이밍 채널 이름',
+    type: 'text',
+  },
+  {
+    climbingId: 8,
+    name: '클라이밍 채널 이름',
+    type: 'voice',
+  },
+  {
+    climbingId: 9,
+    name: '클라이밍 채널 이름',
+    type: 'run',
+  },
+];
+
 const ChannelListPage = () => {
-  const buttonData = [
+  const buttonData: ButtonData[] = [
     { icon: <SCategoryAdd />, label: '분류 추가' },
     { icon: <SChannelAdd />, label: '모임 추가' },
     { icon: <SEdit />, label: '목록 편집' },
   ];
 
-  const channelNameData = [
-    { name: '나의 등반', state: 'open', children: <Carousel /> },
-    { name: '모집 중인 등반', state: 'close', children: <ChannelList /> },
-    { name: '진행 중인 등반', state: 'open', children: <ChannelList /> },
-    { name: '종료된 등반', state: 'close' },
+  const channelNameData: ChannelData[] = [
+    { name: '나의 등반', children: <Carousel /> },
+    {
+      name: '모집 중인 등반',
+      children: <ChannelList list={mockList} />,
+    },
+    {
+      name: '진행 중인 등반',
+      children: <ChannelList list={mockList} />,
+    },
+    {
+      name: '종료된 등반',
+      children: <ChannelList list={mockList} />,
+    },
   ];
 
   return (
@@ -35,7 +78,7 @@ const ChannelListPage = () => {
         </SButtonContainer>
         <SContainer>
           {channelNameData.map((data) => (
-            <Accordion key={data.name} text={data.name} state={data.state}>
+            <Accordion key={data.name} text={data.name}>
               {data.children}
             </Accordion>
           ))}
@@ -59,8 +102,8 @@ const SButtonContainer = styled.div`
   justify-content: space-between;
   gap: 10px;
 `;
-const IconSize = styled.style`
-  width: 5px;
+const IconSize = css`
+  width: 1rem;
   height: 1rem;
 `;
 const SEdit = styled(Edit)`
