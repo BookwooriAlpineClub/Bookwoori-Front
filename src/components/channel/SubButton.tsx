76e645acc86441b54f4e1b93b3ffcccda4c61972
rwaeng <rwaeng0421@gmail.com>
2024-10-29 17:54:48 +0900

    💄 채널 리스트 상단 버튼 퍼블리싱 (#18)

---
+++ src/components/channel/SubButton.tsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+const SubButton = ({ children }: { children: React.ReactNode }) => {
+  return <SButton>{children}</SButton>;
+};
+
+export default SubButton;
+
+const SButton = styled.button`
+  display: flex;
+  padding: 8px 15px;
+  justify-content: center;
+  align-items: center;
+  gap: 0.3125rem;
+
+  width: calc(100% - 30px);
+
+  border-radius: 0.9375rem;
+  background-color: ${({ theme }) => theme.colors.blue100};
+
+  ${({ theme }) => theme.fonts.body}
+  color: ${({ theme }) => theme.colors.white};
+`;
