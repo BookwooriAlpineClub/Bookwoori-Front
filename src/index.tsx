35d9a37f5ba5ac84efd1f428b805904fce9ee40b
Hyerin Sung <hyerinx@naver.com>
2024-10-31 01:07:49 +0900

    🚨 별칭 경로 적용 (#6)

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
 import GlobalStyle from './styles/global';
 import Router from './Router';
-import Toast from './components/common/Toast';
+import Toast from '@src/components/common/Toast';
 
 const queryClient = new QueryClient();
 
