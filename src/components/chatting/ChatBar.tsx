587d38fb43255f77356ee9fd488ee6e06e594e5a
rwaeng <rwaeng0421@gmail.com>
2024-10-30 23:11:22 +0900

    💄 채팅 페이지 하단 채팅바 컴포넌트 퍼블리싱 (#36)

---
+++ src/components/chatting/ChatBar.tsx
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+import { useState } from 'react';
+import { ReactComponent as Send } from '@src/assets/icons/send.svg';
+import { ReactComponent as SendGreen } from '@src/assets/icons/send_green.svg';
+
+const ChatBar = ({ nickname }: { nickname: string }) => {
+  const [chat, setChat] = useState<string>('');
+
+  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setChat(e.target.value);
+  };
+
+  return (
+    <SLayout>
+      <SInput
+        type='text'
+        value={chat}
+        onChange={handleChangeInput}
+        placeholder={`${nickname}에게 문자 보내기`}
+      />
+      {chat ? <SendGreen /> : <Send />}
+    </SLayout>
+  );
+};
+
+export default ChatBar;
+
+const SLayout = styled.div`
+  display: flex;
+  gap: 0.625rem;
+  position: absolute;
+  bottom: 0;
+
+  width: 100%;
+  padding: 0.9375rem;
+
+  background-color: ${({ theme }) => theme.colors.white};
+`;
+const SInput = styled.input`
+  padding: 0 0.625rem;
+  width: 100%;
+
+  border-radius: 1.875rem;
+
+  ${({ theme }) => theme.fonts.body};
+  background-color: ${({ theme }) => theme.colors.black300};
+`;
