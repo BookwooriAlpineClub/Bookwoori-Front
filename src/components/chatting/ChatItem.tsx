7c6fb554dfc82c0f4fc57b293d817fbd46638209
rwaeng <rwaeng0421@gmail.com>
2024-10-31 00:03:33 +0900

    💄 채팅 아이템(말풍선) 컴포넌트 추가 (#36)

---
+++ src/components/chatting/ChatItem.tsx
@@ -0,0 +1,60 @@
+import styled from 'styled-components';
+
+interface chatProps {
+  nickname: string;
+  time: string;
+  text: string;
+}
+
+const ChatItem = ({ nickname, time, text }: chatProps) => {
+  return (
+    <SLayout>
+      <SImg />
+      <SContainer>
+        <SWrapper>
+          <SNickname>{nickname}</SNickname>
+          <STime>{time}</STime>
+        </SWrapper>
+        <SText>{text}</SText>
+      </SContainer>
+    </SLayout>
+  );
+};
+
+export default ChatItem;
+
+const SLayout = styled.div`
+  display: flex;
+  gap: 0.75rem;
+
+  padding: 0.9375rem 1.25rem;
+`;
+const SImg = styled.img`
+  width: 40px;
+  height: 40px;
+
+  border-radius: 50%;
+  background-color: ${({ theme }) => theme.colors.blue300};
+`;
+const SContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.25rem;
+`;
+const SWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+`;
+const SNickname = styled.label`
+  line-height: 20px;
+`;
+const STime = styled.label`
+  ${({ theme }) => theme.fonts.caption};
+  color: var(--400, #9496a1);
+`;
+const SText = styled.p`
+  ${({ theme }) => theme.colors.black100};
+  line-height: 20px;
+  font-weight: 600;
+`;
