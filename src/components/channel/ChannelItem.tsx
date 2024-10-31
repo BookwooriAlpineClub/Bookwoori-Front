444b8194d0e71aeb944cedb81da0a9e1b7fa6685
rwaeng <rwaeng0421@gmail.com>
2024-11-01 01:56:08 +0900

    💄 채널 아이템 컴포넌트 색상 변경 경우 추가 (#18)

--- src/components/channel/ChannelItem.tsx
+++ src/components/channel/ChannelItem.tsx
@@ -4,19 +4,20 @@ import { ReactComponent as Voice } from '@src/assets/icons/voice.svg';
 import { ReactComponent as Run } from '@src/assets/icons/run.svg';
 
 interface Props {
+  color?: string;
   type: string;
   children: React.ReactNode;
 }
 
-const ChannelItem = ({ type, children }: Props) => {
+const ChannelItem = ({ color = 'black', type, children }: Props) => {
   const iconMapping: { [key: string]: React.ReactNode } = {
-    text: <SHash />,
-    voice: <SVoice />,
-    run: <SRun />,
+    text: <SHash $color={color} />,
+    voice: <SVoice $color={color} />,
+    run: <SRun $color={color} />,
   };
 
   return (
-    <SItem>
+    <SItem $color={color}>
       {iconMapping[type]} {children}
     </SItem>
   );
@@ -24,21 +25,25 @@ const ChannelItem = ({ type, children }: Props) => {
 
 export default ChannelItem;
 
-const SItem = styled.div`
+const SItem = styled.div<{ $color: string }>`
   display: flex;
   gap: 0.625rem;
 
   padding: 0 0.625rem;
+
+  color: ${({ theme, $color }) =>
+    $color === 'grey' ? theme.colors.black200 : theme.colors.black100};
 `;
-const iconStyle = css`
-  fill: ${({ theme }) => theme.colors.black100};
+const iconStyle = css<{ $color: string }>`
+  fill: ${({ theme, $color }) =>
+    $color === 'grey' ? theme.colors.black200 : theme.colors.black100};
 `;
-const SHash = styled(Hash)`
+const SHash = styled(Hash)<{ $color: string }>`
   ${iconStyle}
 `;
-const SVoice = styled(Voice)`
+const SVoice = styled(Voice)<{ $color: string }>`
   ${iconStyle}
 `;
-const SRun = styled(Run)`
+const SRun = styled(Run)<{ $color: string }>`
   ${iconStyle}
 `;
