36d7713a177977df9043fabc4cd1d5385bfd8f42
yyeonzu <tooma22@naver.com>
2024-11-02 01:49:54 +0900

    :hammer: test code 작성 시 사용하는 Provider 컴포넌트 (#26) - ThemeProvider를 감싼 상태로 render하는 Provider 함수 - eslint 중 @testing-library/react의 import 위치 규칙 위반 (빌드는 ok) - 혜린님의 공통 코드로 대체될 예정

---
+++ src/tests/utils/renderWithProvider.setup.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { ThemeProvider } from 'styled-components';
+import { theme } from '@src/styles/theme';
+
+const renderWithProviders = (ui: React.ReactElement) => {
+  return render(<ThemeProvider theme={theme}>{ui}</ThemeProvider>);
+};
+
+export default renderWithProviders;
