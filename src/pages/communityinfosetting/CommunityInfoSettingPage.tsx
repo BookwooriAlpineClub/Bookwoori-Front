b7758e0e2b5eb52d1f9bae8f785e2ecb11b9e66c
yyeonzu <tooma22@naver.com>
2024-11-02 02:37:12 +0900

    :clown_face:  API response 타입 정의 & 목데이터 (#26) - local scope 정의 - API 명세서와 동일한 양식

--- src/pages/communityinfosetting/CommunityInfoSettingPage.tsx
+++ src/pages/communityinfosetting/CommunityInfoSettingPage.tsx
@@ -1,3 +1,12 @@
+interface ApiResponse {
+  name: string;
+  adminName: string; // 방장 이름
+  memberCount: number; // 멤버 수
+  creationDate: string;
+  description: string;
+  imageUrl: string;
+}
+
 export interface CommunityInfoType {
   name: string;
   memberInfo: string; // 방장 이름 + 멤버 수
@@ -8,3 +17,16 @@ export interface CommunityInfoType {
 
 export type CommunityRoleType = 'admin' | 'user';
 
+const mockApiResponse: ApiResponse = {
+  name: '피크민을 하자',
+  adminName: '일이삼사오육칠팔구십',
+  memberCount: 25,
+  creationDate: '2000.99.99',
+  imageUrl: '/path/to/image.jpg',
+  description:
+    '일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이' +
+    '삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사' +
+    '오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육' +
+    '칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔' +
+    '구십일이삼사오육칠팔구십',
+};
