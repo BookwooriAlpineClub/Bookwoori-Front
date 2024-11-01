a7b7a26236068339fd2b26e12bacb39e2218a82a
yyeonzu <tooma22@naver.com>
2024-11-02 01:46:06 +0900

    :poop: 빈 함수 export (#26) 한 파일 이내에서 단일 모듈의 named export가 eslint에 위배되어 empty 모듈 생성. 삭제 예정 코드

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,6 +1,9 @@
-export {};
 // ms초 만큼 기다리는 함수
 const delay = (ms: number): Promise<void> =>
   new Promise((resolve) => {
     setTimeout(resolve, ms);
   });
+
+const empty = {};
+
+export { delay, empty };
