6d5afeed40435bc9b5750dd0e25ffa51319dd272
yyeonzu <tooma22@naver.com>
2024-11-02 02:09:25 +0900

    :hammer: CommunitySettingSection 컴포넌트 Props 정의 (#26)

---
+++ src/components/communityinfosetting/CommunitySettingSection.tsx
@@ -0,0 +1,5 @@
+import { CommunityRoleType } from '@src/pages/communityinfosetting/CommunityInfoSettingPage';
+
+export interface CommunitySettingSectionProps {
+  communityRole: CommunityRoleType;
+}
