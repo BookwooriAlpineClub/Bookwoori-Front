b0aed4a0081e7273330e926770640cfbede03157
Hyerin Sung <hyerinx@naver.com>
2024-11-02 17:58:00 +0900

    ♻️ 코드 재활용을 위한 리팩토링 (#7)

--- src/hooks/useDialog.ts
+++ src/hooks/useDialog.ts
@@ -1,34 +1,13 @@
+import useModal from '@src/hooks/useModal';
 import { useSetRecoilState } from 'recoil';
 import { dialogState } from '@src/states/atoms';
 
 const useDialog = () => {
   const setDialog = useSetRecoilState(dialogState);
 
-  const openDialog = (content: React.ReactNode): void => {
-    setDialog((prev) => {
-      return { ...prev, isOpen: true, content };
-    });
-    setTimeout(
-      () =>
-        setDialog((prev) => {
-          return { ...prev, transition: 'open' };
-        }),
-      30,
-    );
-  };
-
-  const closeDialog = (): void => {
-    setDialog((prev) => {
-      return { ...prev, transition: 'close' };
-    });
-    setTimeout(
-      () =>
-        setDialog((prev) => {
-          return { ...prev, isOpen: false, content: '' };
-        }),
-      300,
-    );
-  };
+  const { openModal, closeModal } = useModal(setDialog, 300);
+  const openDialog = openModal;
+  const closeDialog = closeModal;
 
   return { openDialog, closeDialog };
 };
