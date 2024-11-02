adb80e09e18e37a1f6c78f48c2392b62ed13f198
Hyerin Sung <hyerinx@naver.com>
2024-11-02 23:53:53 +0900

    🐛 스크롤 금지/허용 관련 버그 수정 (#8)

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -2,6 +2,11 @@ import type { SetterOrUpdater } from 'recoil';
 
 const useModal = (setModal: SetterOrUpdater<Modal>, transitionMs: number) => {
   const openModal = (content: React.ReactNode): void => {
+    // 스크롤 금지
+    document.body.style.cssText = `
+      position: fixed;
+      top: -${window.scrollY}px;
+    `;
     // 마운트
     setModal((prev) => {
       return { ...prev, content, isOpen: true };
@@ -24,6 +29,10 @@ const useModal = (setModal: SetterOrUpdater<Modal>, transitionMs: number) => {
       setModal((prev) => {
         return { ...prev, content: '', isOpen: false };
       });
+      // 스크롤 허용
+      const scrollY: number = Number(document.body.style.top.slice(1, -2));
+      document.body.style.cssText = '';
+      window.scrollTo(0, scrollY);
     }, transitionMs);
   };
 
