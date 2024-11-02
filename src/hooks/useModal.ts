6237b7a330b5d4ea9214c3bc17209427edda7ebf
Hyerin Sung <hyerinx@naver.com>
2024-11-02 23:51:40 +0900

    💡 useModal 주석 추가 (#8)

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -2,10 +2,12 @@ import type { SetterOrUpdater } from 'recoil';
 
 const useModal = (setModal: SetterOrUpdater<Modal>, transitionMs: number) => {
   const openModal = (content: React.ReactNode): void => {
+    // 마운트
     setModal((prev) => {
       return { ...prev, content, isOpen: true };
     });
     setTimeout(() => {
+      // 트랜지션
       setModal((prev) => {
         return { ...prev, transition: 'open' };
       });
@@ -13,10 +15,12 @@ const useModal = (setModal: SetterOrUpdater<Modal>, transitionMs: number) => {
   };
 
   const closeModal = (): void => {
+    // 트랜지션
     setModal((prev) => {
       return { ...prev, transition: 'close' };
     });
     setTimeout(() => {
+      // 언마운트
       setModal((prev) => {
         return { ...prev, content: '', isOpen: false };
       });
