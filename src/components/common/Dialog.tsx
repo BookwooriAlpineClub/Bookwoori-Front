009d005990079f588142a47ff2c14111e19dd705
Hyerin Sung <hyerinx@naver.com>
2024-11-02 17:34:00 +0900

    💡 Dialog 컴포넌트 사용법 수정 (#7)

--- src/components/common/Dialog.tsx
+++ src/components/common/Dialog.tsx
@@ -1,5 +1,18 @@
 /*
 Dialog 컴포넌트 사용법
+
+1. useDialog 훅을 불러온다.
+import useDialog from '@src/hooks/useDialog';
+const { openDialog, closeDialog } = useDialog();
+
+2. openDialog()로 열고, closeDialog()로 닫는다.
+const ConfirmDialog: React.ReactNode = (
+  <div>
+    <button type='button' onClick={closeDialog}>취소</button>
+    <button type='button' onClick={() => {action(); closeDialog();}}>확인</button>
+  </div>
+);
+openDialog(ConfirmDialog);
 */
 
 import styled from 'styled-components';
