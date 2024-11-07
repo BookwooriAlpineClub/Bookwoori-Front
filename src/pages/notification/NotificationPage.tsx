bf131c1214b0b3966794794027b250e479a601e8
Hyerin Sung <hyerinx@naver.com>
2024-11-08 02:13:38 +0900

    💄 알림 페이지 퍼블리싱 (#20)

--- src/pages/notification/NotificationPage.tsx
+++ src/pages/notification/NotificationPage.tsx
@@ -1,5 +1,98 @@
+import { useEffect, useState } from 'react';
+import { formatTimeGap } from '@src/utils/formatters';
+import styled from 'styled-components';
+import Header from '@src/components/common/Header';
+import BadgeListItem from '@src/components/common/BadgeListItem';
+
+const mock: BadgeItem[] = [
+  {
+    imgUrl: '/',
+    caption: 'AAA공동체 - BBB모임',
+    time: '2024-11-08 02:05:00',
+    message: 'OO님이 나를 언급했어요.',
+    isRead: false,
+  },
+  {
+    imgUrl: '/',
+    caption: 'AAA공동체 - BBB모임',
+    time: '2024-11-08 02:04:30',
+    message: 'OO님이 나를 언급했어요.',
+    isRead: false,
+  },
+  {
+    imgUrl: '/',
+    caption: 'AAA공동체 - BBB모임',
+    time: '2024-11-08 01:50:59',
+    message: 'OO님이 나를 언급했어요.',
+    isRead: false,
+  },
+  {
+    imgUrl: '/',
+    caption: 'AAA공동체 - BBB모임',
+    time: '2024-11-08 01:00:00',
+    message: 'OO님이 나를 언급했어요.',
+    isRead: false,
+  },
+  {
+    imgUrl: '/',
+    caption: 'AAA공동체 - BBB모임',
+    time: '2024-10-21 01:00:00',
+    message: 'OO님이 나를 언급했어요.',
+    isRead: false,
+  },
+  {
+    imgUrl: '/',
+    caption: 'CCC공동체 - DDD모임',
+    time: '2024-09-07 23:00:00',
+    message: '등반 모임 생성이 완료됐어요.',
+    isRead: true,
+  },
+  {
+    imgUrl: '/',
+    caption: 'EEE공동체 - FFF모임',
+    time: '2020-11-07 00:00:00',
+    message: 'OO님이 내 감상평에 반응을 남겼어요.',
+    isRead: true,
+  },
+];
+
 const NotificationPage = () => {
-  return <div />;
+  const [notiList, setNotiList] = useState<BadgeItem[]>([]);
+
+  useEffect(() => {
+    const data = mock; // 데이터 fetch
+    setNotiList(
+      data.map((item) => {
+        const time = formatTimeGap(new Date(), new Date(item.time));
+        return { ...item, time };
+      }),
+    );
+  }, []);
+
+  return (
+    <Layout>
+      <Header text='알림' headerType='hamburger' />
+      <Ul>
+        {notiList.map((item) => (
+          <li key={item.time}>
+            <BadgeListItem type='notice' {...item} />
+          </li>
+        ))}
+      </Ul>
+    </Layout>
+  );
 };
 
 export default NotificationPage;
+
+const Layout = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+`;
+const Ul = styled.ul`
+  display: flex;
+  flex-flow: column nowrap;
+  gap: 0.62rem;
+
+  margin: 0.94rem 5%;
+`;
