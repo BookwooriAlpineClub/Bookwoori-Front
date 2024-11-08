17c4c57c7cea7f8acdb5207f72df5111b00e4a16
yyeonzu <tooma22@naver.com>
2024-11-09 03:03:19 +0900

    :recycle: CommunityInfoCard 컴포넌트 생성(#26) 다른 페이지에서도 공동체 정보 보기 카드가 사용되어 공통 컴포넌트로 수정

--- src/components/communityinfosetting/CommunityInfoSection.tsx
+++ src/components/communityinfosetting/CommunityInfoSection.tsx
@@ -1,5 +1,5 @@
-import styled from 'styled-components';
 import TitleAndFieldContainer from '@src/components/common/TitleAndFieldContainer';
+import CommunityInfoCard from '@src/components/common/CommunityInfoCard';
 
 interface CommunityInfoProps {
   name: string;
@@ -19,74 +19,15 @@ const CommunityInfoSection = ({
   const subtitle = '공동체 정보';
   return (
     <TitleAndFieldContainer title={subtitle}>
-      <CardContainer>
-        <ImageWrapper>
-          <img src={imageUrl} alt={`${name} profile`} />
-        </ImageWrapper>
-        <ContentWrapper>
-          <Name>{name}</Name>
-          <MemberInfo>{memberInfo}</MemberInfo>
-          <Caption>{creationDate}</Caption>
-          <Caption>{description}</Caption>
-        </ContentWrapper>
-      </CardContainer>
+      <CommunityInfoCard
+        name={name}
+        memberInfo={memberInfo}
+        creationDate={creationDate}
+        description={description}
+        imageUrl={imageUrl}
+      />
     </TitleAndFieldContainer>
   );
 };
 
 export default CommunityInfoSection;
-
-const CardContainer = styled.section`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  gap: 1.25rem;
-  width: 100%;
-  border-radius: 1.25rem;
-  background-color: ${({ theme }) => theme.colors.white};
-  padding: 2.19rem 1.88rem;
-`;
-
-const ImageWrapper = styled.div`
-  width: 9.375rem;
-  height: 9.375rem;
-  padding: 0.875rem 0.625rem;
-  justify-content: space-between;
-  align-items: center;
-  overflow: hidden;
-  border-radius: 0.625rem;
-  background-color: ${({ theme }) => theme.colors.black200};
-
-  img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    font-family: ${({ theme }) => theme.fonts.caption};
-    color: ${({ theme }) => theme.colors.black300};
-  }
-`;
-
-const ContentWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  gap: 0.62rem;
-  width: 100%;
-`;
-
-const Name = styled.span`
-  font-family: ${({ theme }) => theme.fonts.nickname};
-  color: ${({ theme }) => theme.colors.black100};
-`;
-
-const MemberInfo = styled.span`
-  font-family: ${({ theme }) => theme.fonts.body};
-  color: ${({ theme }) => theme.colors.blue100};
-`;
-
-const Caption = styled.span`
-  font-family: ${({ theme }) => theme.fonts.caption};
-  color: ${({ theme }) => theme.colors.black200};
-`;
