b3c4c270e54de83f747f58600a42e9aea54d1bdd
yyeonzu <tooma22@naver.com>
2024-11-10 14:17:13 +0900

    :lipstick: 공통 컴포넌트 SubButton 퍼블리싱 (#64)
    - channel list에서 사용되던 SButton을 차용
    - props로 icon, label, width 를 전달 받음
    - icon과 width는 선택사항
    - width의 기본값은 100% - 1.875rem (SButton과 동일)

---
+++ src/components/common/SubButton.tsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+interface SubButtonProps {
+  icon?: React.ReactNode;
+  label: string;
+  width?: string;
+}
+
+const SubButton = ({ icon, label, width }: SubButtonProps) => {
+  return (
+    <Button width={width}>
+      {icon}
+      {label}
+    </Button>
+  );
+};
+
+export default SubButton;
+
+const Button = styled.button<{ width?: string }>`
+  display: flex;
+  padding: 0.5rem 0.9375rem;
+  justify-content: center;
+  align-items: center;
+  gap: 0.3125rem;
+  width: ${({ width }) => width || 'calc(100% - 1.875rem)'};
+  border-radius: 0.9375rem;
+  background-color: ${({ theme }) => theme.colors.blue100};
+  ${({ theme }) => theme.fonts.body}
+  color: ${({ theme }) => theme.colors.white};
+`;
