834e2e21bbf50f8b9662faf3c598aec580c3ef84
yyeonzu <tooma22@naver.com>
2024-11-10 14:15:37 +0900

    :bulb: useCopyToClipboard 사용법 추가 (#64)

--- src/hooks/useCopyToClipboard.ts
+++ src/hooks/useCopyToClipboard.ts
@@ -1,3 +1,9 @@
+/*
+복사하고 싶은 string을 인자로 받아서 클립보드에 복사하는 기능을 하는 hook입니다.
+isCopied : 복사가 완료되면 true, 1초 후 false로 변경됩니다.
+handleCopy : 클립보드에 복사하는 함수입니다. 버튼의 onClick, onChange 등에 사용하면 됩니다.
+ */
+
 import { useState } from 'react';
 
 const useCopyToClipboard = (copyText: string) => {
