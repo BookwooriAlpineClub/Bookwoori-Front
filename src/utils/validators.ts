6180a8816ebffbafc82e40873dea4ea68e532598
yyeonzu <tooma22@naver.com>
2024-11-13 19:20:20 +0900

    :sparkles: base64 인코딩 유효성 검사 함수 base64로 인코딩된 값일 경우 return true

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -1,1 +1,8 @@
-export {};
+/* eslint-disable */
+export const isBase64Encoded = (str: string): boolean => {
+  try {
+    return btoa(atob(str)) === str;
+  } catch {
+    return false; // 디코딩 실패 시 Base64가 아님
+  }
+};
