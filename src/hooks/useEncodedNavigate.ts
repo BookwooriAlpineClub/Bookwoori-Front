fa73b7586765bc557477489e6c4c0299b74feb5b
yyeonzu <yyeonzuu@gmail.com>
2024-11-21 14:40:20 +0900

    :sparkles: 매개변수에 useNavigate option 추가 (#73)

--- src/hooks/useEncodedNavigate.ts
+++ src/hooks/useEncodedNavigate.ts
@@ -1,13 +1,17 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, NavigateOptions } from 'react-router-dom';
 import { encodeId } from '@src/utils/formatters';
 
 // Base64 인코딩된 ID로 네비게이션
 const useEncodedNavigation = () => {
   const navigate = useNavigate();
 
-  const navigateWithEncodedId = (path: string, id: number) => {
+  const navigateWithEncodedId = (
+    path: string,
+    id: number,
+    options?: NavigateOptions,
+  ) => {
     const encodedId = encodeId(id);
-    navigate(`${path}/${encodedId}`);
+    navigate(`${path}/${encodedId}`, options);
   };
 
   return navigateWithEncodedId;
