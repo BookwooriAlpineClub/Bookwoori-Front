a971af7511c95801e236f9c17f8e7acc0c18d20b
rwaeng <rwaeng0421@gmail.com>
2024-11-23 13:59:44 +0900

    🐛 토큰 재발급 요청 api 바디 값으로 쿠키 추가 (#89)

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,5 +1,5 @@
+import { AxiosError } from 'axios';
 import client from '@src/apis/client';
-
 import authClient from './authClient';
 
 // const postLogin = async () => {
@@ -14,21 +14,24 @@ import authClient from './authClient';
 // };
 
 const postLogout = async () => {
-    try {
-      await authClient.post('auth/logout');
-    } catch (e) {
-      if (e instanceof Error) {
-        throw new Error(e.message);
-      }
+  try {
+    await authClient.post('auth/logout');
+  } catch (e) {
+    if (e instanceof Error) {
+      throw new Error(e.message);
     }
+  }
 };
 
 const postRefreshToken = async () => {
   try {
-    const res = await client.post('auth/token');
+    const cookie = document.cookie.split('=');
+    const res = await client.post('auth/token', {
+      refreshToken: cookie[1],
+    });
     localStorage.setItem('accessToken', res.data.accessToken);
   } catch (e) {
-    if (e instanceof Error) {
+    if (e instanceof AxiosError) {
       throw new Error(e.message);
     }
   }
