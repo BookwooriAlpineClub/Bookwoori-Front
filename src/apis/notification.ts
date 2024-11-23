3e07c2ca30aab6bc4aa68e6a9539706aafa644f7
Hyerin Sung <hyerinx@naver.com>
2024-11-23 21:13:56 +0900

    🔨 Notification API 도메인 + 타입 추가

---
+++ src/apis/notification.ts
@@ -0,0 +1,35 @@
+import type { AxiosResponse } from 'axios';
+import type { Notification } from '@src/types/apis/notification';
+import { authClient } from '@src/apis/index';
+
+// 알림 구독
+// 백엔드 API 명세서 미완성
+// export const getNotification = async <Res>(): Promise<Res> => {
+//   const response = await authClient.get<Res>(`/notification`);
+//   return response.data;
+// };
+
+// 알림 리스트 조회
+export const getNotificationList = async <
+  Res = Notification[],
+>(): Promise<Res> => {
+  const response = await authClient.get<Res>(`/notification`);
+  return response.data;
+};
+
+// 알림 읽기
+// 백엔드 API 명세서 미완성
+// export const patchNotification = async <
+//   Res = void,
+//   Req = Pick<Notification, 'isRead'>,
+// >(
+//   id: number,
+//   body: Req,
+// ): Promise<Res> => {
+//   const response = await authClient.patch<Res, AxiosResponse<Res>, Req>(
+//     `/notification/${id}`,
+//     body,
+//     { headers: { 'Content-Type': 'application/json' } },
+//   );
+//   return response.data;
+// };
