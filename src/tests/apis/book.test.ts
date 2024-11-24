109ccd21f396f0e16ba827d09185c6ce6dd230b1
Hyerin Sung <hyerinx@naver.com>
2024-11-24 23:56:33 +0900

    ✅ Book API 테스트 파일 수정

--- src/tests/apis/book.test.ts
+++ src/tests/apis/book.test.ts
@@ -1,12 +1,17 @@
-import { Book } from '@src/types/apis/book.d';
-import { getBookList, getBookItem } from '@src/apis/book';
+/**
+ * @jest-environment node
+ * @jest-environment-options {}
+ */
+
+import type { BookListItem, BookDetail } from '@src/types/apis/book.d';
+import { getBookList, getBookDetail } from '@src/apis/book';
 
 describe('Book API', () => {
   test('getBookList', async () => {
     const keyword: string = '한강';
-    const data: Book[] = await getBookList(keyword);
+    const data: BookListItem[] = await getBookList(keyword);
 
-    expect(data).toContain<Book>({
+    expect(data).toContain<BookListItem>({
       title: '디 에센셜 한강 (무선 보급판) - 2024 노벨문학상 수상작가',
       author: '한강 (지은이)',
       publisher: '문학동네',
@@ -16,19 +21,18 @@ describe('Book API', () => {
         'https://image.aladin.co.kr/product/31784/0/coversum/8954693466_2.jpg',
     });
   });
-  test('getBookItem', async () => {
+  test('getBookDetail', async () => {
     const isbn13: number = 9788954693462;
-    const data: Book = await getBookItem(isbn13);
+    const data: BookDetail = await getBookDetail(isbn13);
 
-    expect(data).toMatchObject<Book>({
+    expect(data).toMatchObject<BookDetail>({
       title: '디 에센셜 한강 (무선 보급판) - 2024 노벨문학상 수상작가',
       author: '한강 (지은이)',
       publisher: '문학동네',
-      pubDate: '2023-06-01',
+      pubYear: '2023',
       itemPage: 364,
       description:
         '작가의 핵심 작품들을 큐레이팅하여 한 권으로 엮은 스페셜 에디션 ‘디 에센셜The essential’. 문학동네에서 출시하는 디 에센셜 한국작가 편은 ‘센세이션’이라는 키워드 아래, 독자들에게 강렬한 독서 경험을 선사하며 한국문학에 센세이션을 일으킨 작가를 선정한다. 첫번째 작가는 한강이다.',
-      isbn13: '9788954693462',
       cover:
         'https://image.aladin.co.kr/product/31784/0/coversum/8954693466_2.jpg',
     });
