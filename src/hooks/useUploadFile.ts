67f6b794a8bb11884e499867f47014477c1054ba
rwaeng <rwaeng0421@gmail.com>
2024-11-24 16:44:30 +0900

    🐛 useUploadFile 훅 수정 (#102)

--- src/hooks/useUploadFile.ts
+++ src/hooks/useUploadFile.ts
@@ -1,14 +1,32 @@
 import { useState } from 'react';
+import { RecoilState, useRecoilState } from 'recoil';
+import useToast from './useToast';
 
-const useUploadFile = (previewImg: string | undefined) => {
-  const [file, setFile] = useState<File | null>(null);
+const useUploadFile = (
+  previewImg: string | undefined,
+  fileState: RecoilState<File | null>,
+) => {
+  const [file, setFile] = useRecoilState<File | null>(fileState);
   const [preview, setPreview] = useState<string | undefined>(previewImg);
+  const addToast = useToast();
+
+  const validateMimeTypes = (newFile: File) => {
+    const validMimeTypes = ['image/png', 'image/jpeg'];
+    return validMimeTypes.includes(newFile.type);
+  };
 
   const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
     const selectedFiles = e.target.files;
     if (!selectedFiles?.length) return;
 
     const newFile = selectedFiles[0];
+
+    if (!validateMimeTypes(newFile)) {
+      addToast({ content: '.png, .jpeg의 파일 형식만 지원합니다.' });
+      e.target.value = '';
+      return;
+    }
+
     setFile(newFile);
     setPreview(URL.createObjectURL(newFile));
 
