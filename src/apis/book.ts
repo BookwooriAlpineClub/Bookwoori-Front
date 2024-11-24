fbe9b4b1e63d4c19a69f84c2cf1a6cc3a0028ba5
Hyerin Sung <hyerinx@naver.com>
2024-11-24 22:53:54 +0900

    🏷️ Book API 타입 수정 (#94)

--- src/apis/book.ts
+++ src/apis/book.ts
@@ -1,16 +1,19 @@
-import type { Book } from '@src/types/apis/book.d';
+import type { BookListItem, BookDetail } from '@src/types/apis/book.d';
 import { authClient } from '@src/apis/index';
 
 /** 도서 검색 목록 조회 */
-export const getBookList = async <Res = Book[]>(
+export const getBookList = async <Res = BookListItem[]>(
   keyword: string,
 ): Promise<Res> => {
   const response = await authClient.get<Res>(`/books?keyword=${keyword}`);
   return response.data;
 };
 
-/** 도서 상세 조회 */
-export const getBookDetail = async <Res = Book>(
+/**
+ * 도서 상세 조회
+ * @ 백엔드에 수정 요청함
+ */
+export const getBookDetail = async <Res = BookDetail>(
   isbn13: number,
 ): Promise<Res> => {
   const response = await authClient.get<Res>(`/books/${isbn13}`);
--- src/types/apis/book.d.ts
+++ src/types/apis/book.d.ts
@@ -1,17 +1,15 @@
-export type Book = {
+export type BookDetail = {
   title: string;
   author: string;
   publisher: string;
-  pubDate: string;
+  pubYear: string;
   itemPage: number;
   description: string;
-  isbn13: string;
   cover: string;
 };
-
-export type BookInfo = Pick<
-  Book,
-  'title' | 'author' | 'publisher' | 'itemPage' | 'isbn13' | 'cover'
-> & {
+export type BookListItem = Omit<BookDetail, 'itemPage' | 'description'> & {
+  isbn13: string;
+};
+export type BookInfo = BookDetail & {
   bookId: number;
 };
