88519700292a393507da2af24b21d675c2f00a77
rwaeng <rwaeng0421@gmail.com>
2024-11-25 20:14:37 +0900

    ♻️ member.ts api 요청 코드 수정 (#98)

--- src/apis/member.ts
+++ src/apis/member.ts
@@ -1,28 +1,24 @@
 import { authClient } from '@src/apis/index';
 import { ExpResponse, ProfileResponse } from '@src/types/domain/member';
 
-const patchProfile = async (profileData: FormData) => {
-  try {
-    await authClient.patch('members/me', profileData, {
-      headers: {
-        'Contest-Type': 'multipart/form-data',
-      },
-    });
-  } catch (e) {
-    if (e instanceof Error) {
-      throw new Error(e.message);
-    }
-  }
+/* 프로필 수정 */
+export const patchProfile = async (body: FormData) => {
+  const res = await authClient.patch(`members/me`, body, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  });
+  return res.data;
 };
 
-const getProfile = async (memberId: number): Promise<ProfileResponse> => {
+/* 개별 프로필 조회 */
+export const getProfile = async (memberId: number): Promise<ProfileResponse> => {
   const res = await authClient.get(`members/${memberId}`);
   return res.data;
 };
 
-const getExp = async (): Promise<ExpResponse> => {
+/* 경험치 내역 조회 */
+export const getExp = async (): Promise<ExpResponse> => {
   const res = await authClient.get(`members/me/exp-log`);
   return res.data;
 };
-
-export { patchProfile, getProfile, getExp };
