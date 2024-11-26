1afcf167bf566b87a092046ac6ef4e8cca0b7c43
rwaeng <rwaeng0421@gmail.com>
2024-11-26 13:05:27 +0900

    🐛 쿼리 스트링 사용하는 경우 추가 (#117)

--- src/apis/messageRoom.ts
+++ src/apis/messageRoom.ts
@@ -6,13 +6,27 @@ import {
   MessageRoomRes,
 } from '@src/types/domain/messageRoom';
 
-export const getMessageRoomList = async <
-  Res = MessageRoomListRes,
->(): Promise<Res> => {
-  const response = await authClient.get(`/messageRooms/me`);
+/* 채팅방 목록 조회 */
+export const getMessageRoomList = async <Res = MessageRoomListRes>(
+  page?: number,
+  size?: number,
+): Promise<Res> => {
+  const queryParams = new URLSearchParams();
+
+  if (page) {
+    queryParams.append('page', String(page));
+  }
+
+  if (size) {
+    queryParams.append('size', String(size));
+  }
+
+  const url = `/messageRooms/me${queryParams.toString() ? `?${queryParams.toString()}` : ''}`;
+  const response = await authClient.get(url);
   return response.data;
 };
 
+/* dm 생성 */
 export const postMessageRoom = async <
   Res = MessageRoomRes,
   Req = MessageRoomReq,
@@ -23,7 +37,26 @@ export const postMessageRoom = async <
   return response.data;
 };
 
-export const getDmList = async <Res = DMListRes>(): Promise<Res> => {
-  const response = await authClient.get(`directMessages`);
+/* 채팅 목록 조회 */
+export const getDmList = async <Res = DMListRes>(
+  messageRoomId: number,
+  page?: number,
+  size?: number,
+): Promise<Res> => {
+  const queryParams = new URLSearchParams({
+    messageRoomId: String(messageRoomId),
+  });
+
+  if (page) {
+    queryParams.append('page', String(page));
+  }
+
+  if (size) {
+    queryParams.append('size', String(size));
+  }
+
+  const response = await authClient.get(
+    `directMessages?${queryParams.toString()}`,
+  );
   return response.data;
 };
