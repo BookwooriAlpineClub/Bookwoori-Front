ae83fc974bd9f754b4f66bc5a3250c5f91619138
Hyerin Sung <hyerinx@naver.com>
2024-11-26 20:39:15 +0900

    💄 검색바 추가 (#122)

--- src/pages/book/SearchPage.tsx
+++ src/pages/book/SearchPage.tsx
@@ -1,5 +1,72 @@
+import { useState } from 'react';
+import { useNavigate, createSearchParams } from 'react-router-dom';
+import styled from 'styled-components';
+import { ReactComponent as IcnSearch } from '@src/assets/icons/md_outline_search.svg';
+
 const SearchPage = () => {
-  return <div></div>;
+  const navigate = useNavigate();
+
+  const [keyword, setKeyword] = useState<string>('');
+
+  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    // 새로고침 방지 (기본 기능 비활성화)
+    event.preventDefault();
+
+    // 제출된 데이터 가져오기
+    const formdata = new FormData(event.currentTarget);
+    const input: string = (formdata.get('keyword') ?? '') as string;
+    setKeyword(input);
+
+    // 쿼리스트링 추가 및 삭제
+    if (input)
+      navigate(
+        { search: `?${createSearchParams({ keyword: input })}` },
+        { replace: true },
+      );
+    else navigate({ search: '' }, { replace: true });
+  };
+
+  return (
+    <>
+      <Form onSubmit={handleFormSubmit}>
+        <IcnSearch width={20} height={20} />
+        <Input
+          type='text'
+          name='keyword'
+          placeholder='책 제목, 작가를 검색해 보세요.'
+        />
+      </Form>
+      {keyword}
+    </>
+  );
 };
 
 export default SearchPage;
+
+const Form = styled.form`
+  display: flex;
+  align-items: center;
+  gap: 0.375rem;
+
+  width: 19.375rem;
+  height: 2.5rem;
+  padding: 0.75rem;
+
+  border-radius: 1.875rem;
+  background-color: ${({ theme }) => theme.colors.blue300};
+
+  color: ${({ theme }) => theme.colors.blue100};
+`;
+const Input = styled.input`
+  width: 100%;
+
+  background-color: transparent;
+
+  ${({ theme }) => theme.fonts.body}
+  color: ${({ theme }) => theme.colors.black100};
+  text-overflow: ellipsis;
+
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.blue200};
+  }
+`;
