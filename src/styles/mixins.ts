5be7cf43a4cd754eda73d730efc3f23202c08bd2
Hyerin Sung <hyerinx@naver.com>
2024-11-26 22:02:08 +0900

    💄 ListLayout 믹스인 수정 (#122)

--- src/styles/mixins.ts
+++ src/styles/mixins.ts
@@ -7,6 +7,14 @@ export const NoSelect = css`
   -ms-use-select: none;
   user-select: none;
 `;
+/**
+ * @example
+ * <ListLayout>
+ *   <main>
+ *     <strong>데이터가 없어요.</strong>
+ *   </main>
+ * </ListLayout>
+ */
 export const ListLayout = styled.div`
   display: flex;
   flex-flow: column nowrap;
@@ -14,10 +22,17 @@ export const ListLayout = styled.div`
   height: 100%;
 
   // 데이터 없는 경우 텍스트 표시
+  main:has(strong) {
+    display: flex;
+
+    flex-grow: 1;
+  }
   strong {
     margin: auto;
 
     ${({ theme }) => theme.fonts.body}
     color: ${({ theme }) => theme.colors.black200};
+
+    ${NoSelect}
   }
 `;
