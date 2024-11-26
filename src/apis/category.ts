8a38d366c8fc07ea8bac749033f129757f33ddc2
rwaeng <rwaeng0421@gmail.com>
2024-11-26 12:50:10 +0900

    ♻️ category.ts 형식 수정 (#117)

--- src/apis/category.ts
+++ src/apis/category.ts
@@ -6,16 +6,18 @@ import {
 import { authClient } from '@src/apis/index';
 
 /* 카테고리 생성 */
-export const postCategory = async (data: CategoryRequest) => {
+export const postCategory = async <Res = void, Req = CategoryRequest>(
+  data: Req,
+): Promise<Res> => {
   const res = await authClient.post('categories', data);
   return res.data;
 };
 
 /* 카테고리 이름 변경 */
-export const patchCategory = async (
+export const patchCategory = async <Res = void, Req = CategoryNameRequest>(
   categoryId: CategoryNameRequest,
-  data: CategoryNameRequest,
-) => {
+  data: Req,
+): Promise<Res> => {
   const res = await authClient.patch(`categories/${categoryId}/name`, data);
   return res.data;
 };
@@ -27,10 +29,10 @@ export const deleteCategory = async (categoryId: number) => {
 };
 
 /* 카테고리 위치 변경 */
-export const patchCategoryMove = async (
+export const patchCategoryMove = async <Res = void, Req = CategoryMoveRequest>(
   categoryId: number,
-  data: CategoryMoveRequest,
-) => {
+  data: Req,
+): Promise<Res> => {
   const res = await authClient.patch(`categories/${categoryId}/locate`, data);
   return res.data;
 };
