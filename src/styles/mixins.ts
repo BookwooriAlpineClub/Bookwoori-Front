6a4cd8a4eb63afc4a511b2791399b735702ea97c
Hyerin Sung <hyerinx@naver.com>
2024-11-26 15:41:36 +0900

    🔀 ListLayout 믹스인 (#122)

--- src/styles/mixins.ts
+++ src/styles/mixins.ts
@@ -1,4 +1,4 @@
-import { css } from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const NoSelect = css`
   // 드래그 및 선택 금지
@@ -6,4 +6,18 @@ export const NoSelect = css`
   -moz-user-select: none;
   -ms-use-select: none;
   user-select: none;
+`;
+export const ListLayout = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+
+  height: 100%;
+
+  // 데이터 없는 경우 텍스트 표시
+  strong {
+    margin: auto;
+
+    ${({ theme }) => theme.fonts.body}
+    color: ${({ theme }) => theme.colors.black200};
+  }
 `;
