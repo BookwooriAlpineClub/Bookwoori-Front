eb6864be903278ca7927369f30a0c05d2ef1db54
rwaeng <rwaeng0421@gmail.com>
2024-11-26 12:15:19 +0900

    ✨ 카테고리 api 작성 (#117)

---
+++ src/apis/category.ts
@@ -0,0 +1,36 @@
+import {
+  CategoryMoveRequest,
+  CategoryNameRequest,
+  CategoryRequest,
+} from '@src/types/domain/category';
+import { authClient } from '@src/apis/index';
+
+/* 카테고리 생성 */
+export const postCategory = async (data: CategoryRequest) => {
+  const res = await authClient.post('categories', data);
+  return res.data;
+};
+
+/* 카테고리 이름 변경 */
+export const patchCategory = async (
+  categoryId: CategoryNameRequest,
+  data: CategoryNameRequest,
+) => {
+  const res = await authClient.patch(`categories/${categoryId}/name`, data);
+  return res.data;
+};
+
+/* 카테고리 삭제 */
+export const deleteCategory = async (categoryId: number) => {
+  const res = await authClient.delete(`categories/${categoryId}`);
+  return res.data;
+};
+
+/* 카테고리 위치 변경 */
+export const patchCategoryMove = async (
+  categoryId: number,
+  data: CategoryMoveRequest,
+) => {
+  const res = await authClient.patch(`categories/${categoryId}/locate`, data);
+  return res.data;
+};
