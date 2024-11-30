ee86adbdd566e9f83ec09920a6960e5c2393d9de
Hyerin Sung <hyerinx@naver.com>
2024-11-30 21:42:29 +0900

    💄 라디오 버튼 구현 (#150)

--- src/pages/book/RecordListPage.tsx
+++ src/pages/book/RecordListPage.tsx
@@ -4,35 +4,38 @@ import styled from 'styled-components';
 import { NoDataTextLayout } from '@src/styles/mixins';
 import Header from '@src/components/common/Header';
 
+type Radio = Pick<Record, 'readingStatus'> & {
+  text: '읽고 싶어요' | '읽고 있어요' | '다 읽었어요';
+};
+
 const RecordListPage = () => {
   const [status, setStatus] = useState<Record['readingStatus']>('READING');
+  const radioList: Radio[] = [
+    { readingStatus: 'WISH', text: '읽고 싶어요' },
+    { readingStatus: 'READING', text: '읽고 있어요' },
+    { readingStatus: 'FINISHED', text: '다 읽었어요' },
+  ];
+
   const data: Record[] = [];
 
   return (
     <NoDataTextLayout>
       <Header text='책 기록' headerType='back' />
-      <fieldset name='status'>
-        <input
-          name='status'
-          type='radio'
-          value='읽고 싶어요'
-          onChange={() => setStatus('WISH')}
-        />
-        <input
-          name='status'
-          type='radio'
-          value='읽고 있어요'
-          defaultChecked
-          onChange={() => setStatus('READING')}
-        />
-        <input
-          name='status'
-          type='radio'
-          value='다 읽었어요'
-          onChange={() => setStatus('FINISHED')}
-        />
-        <div />
-      </fieldset>
+      <Fieldset name='status'>
+        {radioList.map(({ readingStatus, text }) => (
+          <Label key={readingStatus}>
+            {text}
+            <input
+              name='status'
+              type='radio'
+              value={readingStatus}
+              onChange={(e) => setStatus(e.target.value as Record['readingStatus'])}
+              defaultChecked={readingStatus === 'READING'}
+            />
+          </Label>
+        ))}
+        <Background status={status} />
+      </Fieldset>
       <main>
         {data.length !== 0 ? (
           <Ul>
@@ -50,6 +53,64 @@ const RecordListPage = () => {
 
 export default RecordListPage;
 
+const Fieldset = styled.fieldset`
+  position: relative;
+
+  display: flex;
+
+  width: 22.0625rem;
+  height: 2.8125rem;
+  margin: 0 auto;
+
+  border: ${({ theme }) => theme.colors.white} 0.25rem solid;
+  border-radius: 62.4375rem;
+  background-color: ${({ theme }) => theme.colors.white};
+
+  overflow: hidden;
+`;
+const Label = styled.label`
+  position: relative;
+  z-index: 3;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 33.3%;
+  height: 100%;
+
+  ${({ theme }) => theme.fonts.mountain}
+  color: ${({ theme }) => theme.colors.blue200};
+
+  &:has(input[type='radio']:checked) {
+    color: ${({ theme }) => theme.colors.white};
+  }
+
+  transition: color 0.3s ease-out;
+`;
+const Background = styled.div<{ status: Record['readingStatus'] }>`
+  position: absolute;
+  left: ${({ status }) => {
+    switch (status) {
+      case 'WISH':
+        return '0';
+      case 'READING':
+        return '33.3%';
+      case 'FINISHED':
+        return '66.8%';
+      default:
+        return '33.3%';
+    }
+  }};
+  z-index: 1;
+
+  width: 33.3%;
+  height: 100%;
+
+  background-color: ${({ theme }) => theme.colors.blue100};
+
+  transition: left 0.3s ease-out;
+`;
 const Ul = styled.ul`
   display: flex;
   flex-flow: row wrap;
