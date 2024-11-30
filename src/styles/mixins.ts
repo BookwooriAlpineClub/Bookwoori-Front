e01abd116fa9e482403de2fe1b2fb3eaeb39098c
Hyerin Sung <hyerinx@naver.com>
2024-11-30 17:08:46 +0900

    🔀 NoDataTextLayout 믹스인 (#150)

--- src/styles/mixins.ts
+++ src/styles/mixins.ts
@@ -1,4 +1,4 @@
-import { css } from 'styled-components';
+import styled, { css } from 'styled-components';
 
 // eslint-disable-next-line import/prefer-default-export
 export const NoSelect = css`
@@ -7,4 +7,39 @@ export const NoSelect = css`
   -moz-user-select: none;
   -ms-use-select: none;
   user-select: none;
+`;
+/**
+ * @example
+ * <NoDataTextLayout>
+ *   <main>
+ *     {data ? (
+ *       // 데이터 표시
+ *     ) : (
+ *       <strong>데이터가 없어요.</strong>
+ *     )}
+ *   </main>
+ * </NoDataTextLayout>
+ */
+export const NoDataTextLayout = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+
+  height: 100%;
+
+  main {
+    padding: 0.94rem 5%;
+  }
+  main:has(strong) {
+    display: flex;
+
+    flex-grow: 1;
+  }
+  strong {
+    margin: auto;
+
+    ${({ theme }) => theme.fonts.body}
+    color: ${({ theme }) => theme.colors.black200};
+
+    ${NoSelect}
+  }
 `;
