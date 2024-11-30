728172265bd5054cf3edd5f086f1b69aefffcd42
Hyerin Sung <hyerinx@naver.com>
2024-11-30 22:14:46 +0900

    🔥 불필요한 ESlint 옵션 삭제 (#150)

--- src/styles/mixins.ts
+++ src/styles/mixins.ts
@@ -1,6 +1,5 @@
 import styled, { css } from 'styled-components';
 
-// eslint-disable-next-line import/prefer-default-export
 export const NoSelect = css`
   // 드래그 및 선택 금지
   -webkit-user-select: none;
