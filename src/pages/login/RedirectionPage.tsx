aea1ff9e780c7ff2322bc8140891d7f74775f590
rwaeng <rwaeng0421@gmail.com>
2024-12-01 20:27:59 +0900

    ✨ 리프레시 토큰 추가 (#161)

--- src/pages/login/RedirectionPage.tsx
+++ src/pages/login/RedirectionPage.tsx
@@ -3,13 +3,14 @@ import { useSearchParams } from 'react-router-dom';
 import { ROUTE_PATH } from '@src/constants/routePath';
 import Spinner from '@src/components/common/Spinner';
 
-
 const RedirectionPage = () => {
   const [searchParams] = useSearchParams();
   const accessToken = searchParams.get('accessToken');
+  const refreshToken = searchParams.get('refreshToken') ?? '';
 
   if (accessToken) {
     localStorage.setItem('accessToken', accessToken);
+    sessionStorage.setItem('refreshToken', refreshToken);
     window.location.replace(ROUTE_PATH.root);
   } else {
     window.location.replace(ROUTE_PATH.signIn);
