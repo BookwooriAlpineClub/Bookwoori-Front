2054531d720e9cef5d33747e4f36416038550823
Hyerin Sung <hyerinx@naver.com>
2024-12-01 10:33:17 +0900

    💄 Tag 컴포넌트 (공통) 퍼블리싱 (#155)

---
+++ src/components/common/Tag.tsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+interface Props {
+  children: string;
+}
+
+const Tag = ({ children }: Props) => {
+  return <Container>{children}</Container>;
+};
+
+export default Tag;
+
+const Container = styled.mark`
+  padding: 0.25rem 0.625rem;
+
+  border-radius: 6.1875rem;
+  border: 1px solid ${({ theme }) => theme.colors.blue100};
+  background-color: ${({ theme }) => theme.colors.blue300};
+
+  ${({ theme }) => theme.fonts.caption}
+  color: ${({ theme }) => theme.colors.blue100};
+`;
