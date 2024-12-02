1c8a43ca1417268367f6a8c8583b916d1eb872da
Hyerin Sung <hyerinx@naver.com>
2024-12-02 22:44:40 +0900

    🔨 책 검색 페이지 API 연결 (#169)

---
+++ src/hooks/query/useBook.ts
@@ -0,0 +1,24 @@
+import type { BookListItem, BookDetail } from '@src/types/apis/book';
+import { useQuery } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
+import { getBookList, getBookDetail } from '@src/apis/book';
+
+interface Props {
+  keyword?: string;
+  isbn13?: string;
+}
+
+const useBook = ({ keyword, isbn13 }: Props) => {
+  const { data: bookList } = useQuery<BookListItem[], AxiosError>({
+    queryKey: ['getBookList', keyword],
+    queryFn: () => getBookList(keyword as string),
+  });
+  const { data: bookDetail } = useQuery<BookDetail, AxiosError>({
+    queryKey: ['getBookDetail', isbn13],
+    queryFn: () => getBookDetail(isbn13 as string),
+  });
+
+  return { bookList, bookDetail };
+};
+
+export default useBook;
