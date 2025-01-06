7a7351a8c605ea9c2cbefb7aee742bb4eb56e83f
Hyerin Sung <hyerinx@naver.com>
2025-01-06 18:27:20 +0900

    🏷️ 로직별 타입 Record 추가 (#206)

---
+++ src/types/record.ts
@@ -0,0 +1,11 @@
+interface Record {
+  readonly recordId: number;
+  status: 'UNREAD' | 'WISH' | 'READING' | 'FINISHED';
+  startDate: string | null;
+  endDate: string | null;
+  currentPage: number | null;
+  starReview: number | null;
+  contentReview: string | null;
+}
+
+export default Record;
