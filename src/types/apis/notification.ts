88dda45e33d59f7409ae654be8609cb4b57166ea
Hyerin Sung <hyerinx@naver.com>
2025-01-07 17:23:58 +0900

    🎨 개별 export (#207)

--- src/types/apis/notification.ts
+++ src/types/apis/notification.ts
@@ -7,23 +7,25 @@ type Success = {
 };
 
 // 기기 등록 조회
-type ResDeviceQuery = Success & {
+export type ResDeviceQuery = Success & {
   result: Device;
 };
 
 // 기기 등록 추가
-type ResDeviceAdd = Success;
-type ReqDeviceAdd = Omit<Device, 'status'>;
+export type ResDeviceAdd = Success;
+export type ReqDeviceAdd = Omit<Device, 'status'>;
 
 // 기기 등록 삭제
-type ResDeviceDelete = Success;
+export type ResDeviceDelete = Success;
 
 // 푸시 알림 pub/sub
+// 백엔드 개발 중
 
 // 안 읽은 알림 pub/sub
+// 백엔드 개발 중
 
 // 알림 목록 조회
+// 백엔드 개발 중
 
 // 알림 isRead 수정
-
-export type { ResDeviceQuery, ResDeviceAdd, ReqDeviceAdd, ResDeviceDelete };
+// 백엔드 개발 중
