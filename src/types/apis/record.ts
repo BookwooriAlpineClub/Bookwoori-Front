59ce982c47f3d1a329fda2085dd17421aed3b18a
Hyerin Sung <hyerinx@naver.com>
2025-01-07 17:38:48 +0900

    🎨 유틸리티 타입을 사용하는 경우가 아니라면 기본적으로 interface로 선언한다. (#207)

--- src/types/apis/record.ts
+++ src/types/apis/record.ts
@@ -2,25 +2,26 @@ import type Book from '@src/types/book';
 import type Record from '@src/types/record';
 
 // 책 기록 목록 조회
-export type RecordListitemQueryRes = Omit<
-  Book,
-  'publisher' | 'pubYear' | 'description' | 'itemPage'
-> & {
+export interface RecordListitemQueryRes
+  extends Omit<Book, 'publisher' | 'pubYear' | 'description' | 'itemPage'> {
   records: NonNullable<
     | Pick<Record, 'recordId' | 'status'>
     | Partial<Pick<Record, 'currentPage' | 'starReview'>>
   >[];
-};
+}
 
 // 책 평가 목록 조회
-export type ReviewListitemQueryRes = Omit<Book, 'description' | 'itemPage'> & {
+export interface ReviewListitemQueryRes
+  extends Omit<Book, 'description' | 'itemPage'> {
   records: NonNullable<
     Pick<Record, 'recordId' | 'status' | 'currentPage' | 'starReview'>
   >[];
-};
+}
 
 // 책 기록 상세 조회
-export type RecordDetailQueryRes = Book & { records: Record[] };
+export interface RecordDetailQueryRes extends Book {
+  records: Record[];
+}
 
 // 책 기록 추가
 export type RecordAddReq = Pick<Book, 'isbn13'> & Omit<Record, 'recordId'>;
