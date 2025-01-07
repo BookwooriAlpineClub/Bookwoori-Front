53bd0c9a4aa80c5bfe96df5dea9a0b6451ce76f9
Hyerin Sung <hyerinx@naver.com>
2025-01-07 22:43:36 +0900

    🔥 불필요한 타입 삭제 (#211)

--- src/types/modal.ts
+++ src/types/modal.ts
@@ -3,21 +3,11 @@ export type Toast = {
   content: string;
   kind?: 'default' | 'success' | 'error';
 };
+
 export type ModalTransition = 'open' | 'close';
+
 export type Modal = {
   content: React.ReactNode;
   isOpen: boolean;
   transition: ModalTransition;
 };
-
-export interface InputProps {
-  title: string;
-  placeholder: string;
-  required: boolean;
-  setValue: React.Dispatch<React.SetStateAction<T>>;
-}
-
-interface SideBar {
-  isOpen: boolean;
-  transition: ModalTransition;
-}
