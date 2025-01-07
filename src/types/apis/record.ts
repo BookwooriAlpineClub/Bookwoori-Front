028ba7c84d0c63eafa6846ff74238a6ce4b71ef1
Hyerin Sung <hyerinx@naver.com>
2025-01-07 17:32:01 +0900

    🎨 네이밍 컨벤션 반영 (#207)

--- src/types/apis/record.ts
+++ src/types/apis/record.ts
@@ -2,7 +2,7 @@ import type Book from '@src/types/book';
 import type Record from '@src/types/record';
 
 // 책 기록 목록 조회
-export type ResRecordListitem = Omit<
+export type RecordListitemQueryRes = Omit<
   Book,
   'publisher' | 'pubYear' | 'description' | 'itemPage'
 > & {
@@ -13,20 +13,20 @@ export type ResRecordListitem = Omit<
 };
 
 // 책 평가 목록 조회
-export type ResReviewListitem = Omit<Book, 'description' | 'itemPage'> & {
+export type ReviewListitemQueryRes = Omit<Book, 'description' | 'itemPage'> & {
   records: NonNullable<
     Pick<Record, 'recordId' | 'status' | 'currentPage' | 'starReview'>
   >[];
 };
 
 // 책 기록 상세 조회
-export type ResRecordDetail = Book & { records: Record[] };
+export type RecordDetailQueryRes = Book & { records: Record[] };
 
 // 책 기록 추가
-export type ReqRecordAdd = Pick<Book, 'isbn13'> & Omit<Record, 'recordId'>;
+export type RecordAddReq = Pick<Book, 'isbn13'> & Omit<Record, 'recordId'>;
 
 // 책 기록 수정
-export type ReqRecordEdit = Partial<Omit<ReqRecordAdd, 'isbn'>>;
+export type RecordEditReq = Partial<Omit<RecordAddReq, 'isbn'>>;
 
 // 책 기록 삭제
 // 없음
