7b96a8cccc89a52e0523a154f791b57b5822ffb2
Hyerin Sung <hyerinx@naver.com>
2025-01-09 21:04:20 +0900

    💡 주석 수정 (#207)

--- src/types/apis/notification.ts
+++ src/types/apis/notification.ts
@@ -5,27 +5,34 @@ interface Success {
   code: number;
   message: string;
 }
-
-// 기기 등록 조회
+/**
+ * 기기 등록 조회
+ */
 export interface DeviceQueryRes extends Success {
   result: Device;
 }
-
-// 기기 등록 추가
+/**
+ * 기기 등록 추가
+ */
 export type DeviceAddRes = Success;
 export type DeviceAddReq = Omit<Device, 'status'>;
-
-// 기기 등록 삭제
+/**
+ * 기기 등록 삭제
+ */
 export type DeviceDeleteRes = Success;
-
-// 푸시 알림 pub/sub
+/**
+ * 푸시 알림 pub/sub
+ */
 // 백엔드 개발 중
-
-// 안 읽은 알림 pub/sub
+/**
+ * 안 읽은 알림 pub/sub
+ */
 // 백엔드 개발 중
-
-// 알림 목록 조회
+/**
+ * 알림 목록 조회
+ */
 // 백엔드 개발 중
-
-// 알림 isRead 수정
+/**
+ * 알림 isRead 수정
+ */
 // 백엔드 개발 중
