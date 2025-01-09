07efb7a1a108d53e628612ab83a7fa11e3760f64
Hyerin Sung <hyerinx@naver.com>
2025-01-09 22:37:57 +0900

    🏷️ 로직 타입 추가 (#206)

--- src/types/record.ts
+++ src/types/record.ts
@@ -1,4 +1,4 @@
-export default interface Record {
+export interface Record {
   readonly recordId: number;
   status: 'UNREAD' | 'WISH' | 'READING' | 'FINISHED';
   startDate: string | null;
@@ -6,4 +6,18 @@ export default interface Record {
   currentPage: number | null;
   starReview: number | null;
   contentReview: string | null;
+}
+
+export interface RecordListitem {
+  readonly recordId: Record['recordId'];
+  status: Record['status'];
+  currentPage?: NonNullable<Record['currentPage']>;
+  starReview?: NonNullable<Record['starReview']>;
+}
+
+export interface ReviewListitem {
+  readonly recordId: Record['recordId'];
+  status: Record['status'];
+  starReview: NonNullable<Record['starReview']>;
+  contentReview: NonNullable<Record['contentReview']>;
 }
