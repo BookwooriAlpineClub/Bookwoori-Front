fbbe99a31deb7111a4205b86c39d39edecb5bbc2
Hyerin Sung <hyerinx@naver.com>
2025-01-10 00:42:44 +0900

    ♻️ API 타입 리팩토링 (#207)

--- src/types/apis/book.ts
+++ src/types/apis/book.ts
@@ -1,9 +1,9 @@
-import type { Book, BookColumnListitem } from '@src/types/book';
+import type Book from '@src/types/book';
 
 /**
  * 도서 검색 목록 조회
  */
-export type GetBookListRes = BookColumnListitem[];
+export type GetBookListRes = Omit<Book, 'description' | 'itemPage'>[];
 /**
  * 도서 상세 조회
  */
