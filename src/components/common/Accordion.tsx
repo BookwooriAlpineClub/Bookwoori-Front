531ce8f61498f047deeaa5888841443131b4364c
rwaeng <rwaeng0421@gmail.com>
2025-01-12 18:01:23 +0900

    ♻️ 아코디언 컴포넌트 리팩토링 (#222)

--- src/components/common/Accordion.tsx
+++ src/components/common/Accordion.tsx
@@ -1,10 +1,11 @@
 import styled from 'styled-components';
-import { ReactComponent as Down } from '@src/assets/icons/down_arrow.svg';
 import React, { useState } from 'react';
+import { ReactComponent as Down } from '@src/assets/icons/hi_outline_chevron_down.svg';
 
 type AccordionProps = {
   id?: number;
-  text: string;
+  title: React.ReactNode;
+  children?: React.ReactNode;
   draggable?: boolean;
   onDragStart?: (e: React.DragEvent, idx: number) => void;
   onDrop?: (idx: number) => void;
@@ -12,12 +13,12 @@ type AccordionProps = {
   // onTouchStart?: (e: React.TouchEvent, idx: number) => void;
   // onTouchEnd?: (e: React.TouchEvent, idx: number) => void;
   // onTouchMove?: (e: React.TouchEvent) => void;
-  children?: React.ReactNode;
 };
 
 const Accordion = ({
   id = -1,
-  text,
+  title,
+  children,
   draggable = false,
   onDragStart = () => {},
   onDrop = () => {},
@@ -25,13 +26,12 @@ const Accordion = ({
   // onTouchStart = () => {},
   // onTouchEnd = () => {},
   // onTouchMove = () => {},
-  children,
 }: AccordionProps) => {
   const [isOpen, setIsOpen] = useState<boolean>(!!children);
   const isTouchDevice = 'ontouchstart' in window;
 
   return (
-    <SLayout
+    <Layout
       draggable={!isTouchDevice && draggable}
       onDragStart={(e) => onDragStart(e, id)}
       onDrop={() => onDrop(id)}
@@ -40,23 +40,20 @@ const Accordion = ({
       // onTouchEnd={(e) => onTouchEnd(e, id)}
       // onTouchMove={onTouchMove}
     >
-      <SContainer>
-        <SLabel>{text}</SLabel>
-        <SButton
-          onClick={() => setIsOpen((prev) => !prev)}
-          disabled={!children}
-        >
+      <Container>
+        {title}
+        <Button onClick={() => setIsOpen((prev) => !prev)} disabled={!children}>
           <SDown $open={isOpen} />
-        </SButton>
-      </SContainer>
+        </Button>
+      </Container>
       {isOpen && children}
-    </SLayout>
+    </Layout>
   );
 };
 
 export default Accordion;
 
-const SLayout = styled.div`
+const Layout = styled.div`
   display: flex;
   flex-direction: column;
   padding: 1.25rem 0.9375rem;
@@ -68,16 +65,12 @@ const SLayout = styled.div`
 
   cursor: pointer;
 `;
-const SContainer = styled.div`
+const Container = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
 `;
-const SLabel = styled.label`
-  ${({ theme }) => theme.fonts.body};
-  color: ${({ theme }) => theme.colors.neutral950};
-`;
-const SButton = styled.button`
+const Button = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
