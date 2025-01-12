648a48ea11e4be456726ab3e708e878f8caa6867
rwaeng <rwaeng0421@gmail.com>
2025-01-12 14:59:16 +0900

    ♻️ IconButton 파일명 변경, 설정 페이지 사용 경우 추가 (#223)

--- src/components/common/IconButton.tsx
+++ src/components/common/IconButton.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
-import biCrown from '@src/assets/icons/bi_crown.svg';
-import fiTrash from '@src/assets/icons/fi_trash_2.svg';
-import fiLogOut from '@src/assets/icons/fi_log_out.svg';
-import bIUserPlus from '@src/assets/icons/bi_user_plus.svg';
-import fiSettings from '@src/assets/icons/fi_settings.svg';
+import { ReactComponent as BiCrown } from '@src/assets/icons/bi_crown.svg';
+import { ReactComponent as FiTrash } from '@src/assets/icons/fi_trash_2.svg';
+import { ReactComponent as FiLogOut } from '@src/assets/icons/fi_log_out.svg';
+import { ReactComponent as BIUserPlus } from '@src/assets/icons/bi_user_plus.svg';
+import { ReactComponent as FiSettings } from '@src/assets/icons/fi_settings.svg';
+import { ReactComponent as HiOutlinePencil } from '@src/assets/icons/hi_outline_pencil.svg';
+import { ReactComponent as HiOutlineFlag } from '@src/assets/icons/hi_outline_flag.svg';
 
 type SettingType = 'transferAuthority' | 'deleteCommunity' | 'leaveCommunity';
 
 type FunctionType = 'copyInvitation' | 'detailInfoSetting';
 
-export type CommunityButtonType = SettingType | FunctionType;
+type UserSettingType = 'editUserInfo' | 'navigateExp' | 'deleteAccount';
 
-interface CommunityButtonProps {
-  type: CommunityButtonType;
+export type IconButtonType = SettingType | FunctionType | UserSettingType;
+
+interface IconButtonProps {
+  type: IconButtonType;
   onClick?: () => void; // 이벤트가 발생하지 않는 버튼 대비 optional 처리
   testId?: string; // 테스트를 하지 않는 버튼 대비 optional 처리
 }
@@ -21,43 +25,49 @@ interface CommunityButtonProps {
 const buttonConfig = {
   transferAuthority: {
     name: '공동체 권한 넘기기',
-    icon: biCrown,
+    icon: <BiCrown />,
   },
   deleteCommunity: {
     name: '공동체 삭제하기',
-    icon: fiTrash,
+    icon: <FiTrash />,
   },
   leaveCommunity: {
     name: '공동체 나가기',
-    icon: fiLogOut,
+    icon: <FiLogOut />,
   },
   copyInvitation: {
     name: '공동체 초대장 복사하기',
-    icon: bIUserPlus,
+    icon: <BIUserPlus />,
   },
   detailInfoSetting: {
     name: '공동체 정보 및 설정 보기',
-    icon: fiSettings,
+    icon: <FiSettings />,
+  },
+  editUserInfo: {
+    name: '인물 정보 수정하기',
+    icon: <HiOutlinePencil />,
+  },
+  navigateExp: {
+    name: '지나온 길 보기',
+    icon: <HiOutlineFlag />,
+  },
+  deleteAccount: {
+    name: '계정 삭제하기',
+    icon: <FiTrash />,
   },
 };
 
-const CommunityButton: React.FC<CommunityButtonProps> = ({
-  type,
-  onClick,
-  testId,
-}) => {
+const IconButton: React.FC<IconButtonProps> = ({ type, onClick, testId }) => {
   const { name, icon } = buttonConfig[type];
   return (
     <ButtonContainer onClick={onClick} data-testid={testId} aria-label={name}>
-      <IconWrapper>
-        <img src={icon} alt={`${name} icon`} />
-      </IconWrapper>
+      <IconWrapper>{icon}</IconWrapper>
       <TextWrapper>{name}</TextWrapper>
     </ButtonContainer>
   );
 };
 
-export default CommunityButton;
+export default IconButton;
 
 const ButtonContainer = styled.button`
   display: flex;
@@ -75,6 +85,7 @@ const ButtonContainer = styled.button`
 const IconWrapper = styled.div`
   width: 1.25rem;
   height: 1.25rem;
+  color: ${({ theme }) => theme.colors.blue700};
 `;
 
 const TextWrapper = styled.span`
