9d7bbba37682cbc68dccb4041a4d4fe4124f6e7a
yyeonzu <yyeonzuu@gmail.com>
2025-01-12 20:41:18 +0900

    :bulb: JSDoc 주석 추가 (#235)

--- src/components/common/ExpandableList.tsx
+++ src/components/common/ExpandableList.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
 import styled from 'styled-components';
 
+/**
+ * Props for the ExpandableList component.
+ * @template T - The type of the items in the list.
+ */
 interface ExpandedListProps<T> {
+  /**
+   * An array of items to display in the list.
+   * @template T - The type of the items in the list.
+   * @type {T[]}
+   */
   items: T[];
+
+  /**
+   * A function to render each item in the list.
+   * @template T - The type of the items in the list.
+   * @param {T} item - The current item in the list.
+   * @returns {React.ReactNode} The rendered JSX for the item.
+   */
   renderItem: (item: T) => React.ReactNode;
 }
 
+/**
+ * A component for rendering a vertically expandable list of items.
+ * Adds a `border-top` style to all items except the first.
+ * @template T - The type of the items in the list.
+ * @param {ExpandedListProps<T>} props - The props for the component.
+ */
 const ExpandableList = <T,>({ items, renderItem }: ExpandedListProps<T>) => {
   return (
     <ListContainer>
