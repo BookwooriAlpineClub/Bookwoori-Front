f8b8738ad641f1b2f3900b551da6570bfe1a735d
Hyerin Sung <hyerinx@naver.com>
2025-01-15 23:11:56 +0900

    🔨 기기 등록 조회, 추가, 삭제 API 요청 메서드 추가 (#199)

--- src/apis/notification.ts
+++ src/apis/notification.ts
@@ -1,25 +1,40 @@
-// import type { AxiosResponse } from 'axios';
+import type { AxiosResponse } from 'axios';
 // import type Notification from '@src/types/notification';
-// import type {
-//   GetDeviceRes,
-//   PostDeviceRes,
-//   PostDeviceReq,
-//   DeleteDeviceRes,
-// } from '@src/types/apis/notification';
-// import { authClient } from '@src/apis/index';
+import type {
+  GetDeviceRes,
+  PostDeviceRes,
+  PostDeviceReq,
+  DeleteDeviceRes,
+} from '@src/types/apis/notification';
+import { authClient } from '@src/apis/index';
 
 /**
  * 기기 등록 조회
  */
-export {};
+export const getDevice = async <Res = GetDeviceRes>(): Promise<Res> => {
+  const response = await authClient.get<Res>(`/notification/devices`);
+  return response.data;
+};
 /**
  * 기기 등록 추가
  */
-export {};
+export const postDevice = async <Res = PostDeviceRes, Req = PostDeviceReq>(
+  body: Req,
+): Promise<Res> => {
+  const response = await authClient.post<Res, AxiosResponse<Res>, Req>(
+    `/notification/devices`,
+    body,
+    { headers: { 'Content-Type': 'application/json' } },
+  );
+  return response.data;
+};
 /**
  * 기기 등록 삭제
  */
-export {};
+export const deleteDevice = async <Res = DeleteDeviceRes>(): Promise<Res> => {
+  const response = await authClient.delete<Res>(`/notification/devices`);
+  return response.data;
+};
 /**
  * 푸시 알림 pub/sub
  */
