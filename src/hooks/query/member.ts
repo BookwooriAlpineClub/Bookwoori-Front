eb5b8d8704f885a47ce13e530f3efc3e0b496531
rwaeng <rwaeng0421@gmail.com>
2025-01-19 17:23:39 +0900

    ♻️ useMember.ts 쿼리 훅 리팩토링 (#253)

---
+++ src/hooks/query/member.ts
@@ -0,0 +1,27 @@
+import { AxiosError } from 'axios';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import type { ProfileRes } from '@src/types/apis/member';
+import { getProfile, patchProfile } from '@src/apis/member';
+
+export const useGetProfile = (userId: number | 'me') => {
+  const { data } = useQuery<ProfileRes, AxiosError>({
+    queryKey: ['getProfile', userId],
+    queryFn: () => getProfile(userId),
+  });
+
+  return { profileData: data };
+};
+
+export const usePatchProfile = () => {
+  const queryClient = useQueryClient();
+
+  const editProfile = useMutation({
+    mutationFn: (formData: FormData) => patchProfile(formData),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['getProfile'] });
+      window.location.reload();
+    },
+  });
+
+  return { editProfile };
+};
