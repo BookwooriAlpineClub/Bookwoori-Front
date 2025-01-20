85b432aae98e72ebe5261e8d3e215960c0fc294f
Hyerin Sung <hyerinx@naver.com>
2025-01-20 18:02:41 +0900

    🔨 설정 페이지에 알림 활성화/비활성화 버튼 추가 (#199)

--- src/pages/userSettings/SettingsPage.tsx
+++ src/pages/userSettings/SettingsPage.tsx
@@ -1,19 +1,54 @@
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 import { ROUTE_PATH } from '@src/constants/routePath';
+import usePermission from '@src/hooks/usePermission';
 import useDialog from '@src/hooks/useDialog';
+import {
+  useGetDevice,
+  usePostDevice,
+  useDeleteDevice,
+} from '@src/hooks/query/useNotification';
 import useMember from '@src/hooks/query/useMember';
 import Header from '@src/components/common/Header';
 import IconButton from '@src/components/common/IconButton';
 import UserProfile from '@src/components/common/UserProfile';
 import DeleteConfirmModal from '@src/components/common/DeleteConfirmModal';
+import { useQueryClient } from '@tanstack/react-query';
 
 const SettingsPage = () => {
   const navigate = useNavigate();
-  const { delAccount } = useMember();
+  const requestNotification = usePermission();
   const { openDialog, closeDialog } = useDialog();
+  const queryClient = useQueryClient();
+  const device = useGetDevice();
+  const createDevice = usePostDevice();
+  const deleteDevice = useDeleteDevice();
+  const { delAccount } = useMember();
 
-  const handleDelete = () => {
+  const handleNotificationOn = async () => {
+    const currentToken = await requestNotification();
+    createDevice.mutate(
+      {
+        body: {
+          platform: 'WEB',
+          token: currentToken,
+        },
+      },
+      {
+        onSuccess() {
+          queryClient.invalidateQueries({ queryKey: ['getDevice'] });
+        },
+      },
+    );
+  };
+  const handleNotificationOff = () => {
+    deleteDevice.mutate(undefined, {
+      onSuccess() {
+        queryClient.invalidateQueries({ queryKey: ['getDevice'] });
+      },
+    });
+  };
+  const handleAccountDelete = () => {
     delAccount.mutate();
   };
 
@@ -27,6 +62,17 @@ const SettingsPage = () => {
             type='editUserInfo'
             onClick={() => navigate(ROUTE_PATH.settingProfile)}
           />
+          {device ? (
+            <IconButton
+              type='notificationTurnOff'
+              onClick={handleNotificationOff}
+            />
+          ) : (
+            <IconButton
+              type='notificationTurnOn'
+              onClick={handleNotificationOn}
+            />
+          )}
           <IconButton
             type='navigateExp'
             onClick={() => navigate(ROUTE_PATH.settingExp)}
@@ -37,7 +83,7 @@ const SettingsPage = () => {
               openDialog(
                 <DeleteConfirmModal
                   closeDialog={closeDialog}
-                  onClickDelete={handleDelete}
+                  onClickDelete={handleAccountDelete}
                 />,
               )
             }
