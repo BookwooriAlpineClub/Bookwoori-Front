d844167c97b462621be7f23dfe323591ab361e6f
Hyerin Sung <hyerinx@naver.com>
2025-01-20 17:02:35 +0900

    🏷️ IconButtonProps 수정 (#199)

--- src/components/common/IconButton.tsx
+++ src/components/common/IconButton.tsx
@@ -10,26 +10,6 @@ import { ReactComponent as FiBell } from '@src/assets/icons/fi_bell.svg';
 import { ReactComponent as HiOutlineFlag } from '@src/assets/icons/hi_outline_flag.svg';
 import { ReactComponent as MdOutlineContentCopy } from '@src/assets/icons/md_outline_content_copy.svg';
 
-type SettingType = 'transferAuthority' | 'deleteCommunity' | 'leaveCommunity';
-
-type FunctionType = 'copyInvitation' | 'detailInfoSetting';
-
-type UserSettingType = 'editUserInfo' | 'navigateExp' | 'deleteAccount';
-
-type EmojiBottomsheetType = 'editMessage' | 'deleteMessage' | 'copyMessage';
-
-export type IconButtonType =
-  | SettingType
-  | FunctionType
-  | UserSettingType
-  | EmojiBottomsheetType;
-
-interface IconButtonProps {
-  type: IconButtonType;
-  onClick?: () => void; // 이벤트가 발생하지 않는 버튼 대비 optional 처리
-  testId?: string; // 테스트를 하지 않는 버튼 대비 optional 처리
-}
-
 const buttonConfig = {
   transferAuthority: {
     name: '공동체 권한 넘기기',
@@ -84,6 +64,12 @@ const buttonConfig = {
     icon: <MdOutlineContentCopy />,
   },
 };
+
+interface IconButtonProps {
+  type: keyof typeof buttonConfig;
+  onClick: () => void;
+  testId?: string; // 테스트를 하지 않는 버튼 대비 optional 처리
+}
 
 const IconButton: React.FC<IconButtonProps> = ({ type, onClick, testId }) => {
   const { name, icon } = buttonConfig[type];
