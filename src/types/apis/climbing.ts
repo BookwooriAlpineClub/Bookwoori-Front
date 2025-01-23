3afacf0c1fdbe2e50c82352eb57777e14bcf985a
yyeonzu <yyeonzuu@gmail.com>
2025-01-23 23:33:44 +0900

    :label: 누락 타입 추가 (#266)

--- src/types/apis/climbing.ts
+++ src/types/apis/climbing.ts
@@ -1,10 +1,20 @@
 import { Climbing, ClimbingMember } from '@src/types/climbing';
 import { EmojiTypeType } from '@src/constants/constants';
 
+export type postClimbingChannelReq = Pick<
+  Climbing,
+  'name' | 'description' | 'startDate' | 'endDate'
+> & {
+  serverId: number;
+  isbn: string;
+};
+
 export type patchClimbingChannelReq = Pick<
   Climbing,
   'name' | 'description' | 'startDate' | 'endDate'
 > & {};
+
+export interface getClimbingInfoRes extends Climbing {}
 
 export type getClimbingChannelMembersRes = {
   climbingMemberList: ClimbingMember[];
