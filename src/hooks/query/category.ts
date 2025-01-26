760cf2b74379ea33a6a4de23944cf5aaaccaeeea
rwaeng <rwaeng0421@gmail.com>
2025-01-26 17:40:55 +0900

    ✨ 카테고리 위치 변경 쿼리 훅 작성 (#254)

--- src/hooks/query/category.ts
+++ src/hooks/query/category.ts
@@ -1,11 +1,11 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
 import { useRecoilValue } from 'recoil';
 import { currentServerIdState } from '@src/states/atoms';
-import { getServerChannels } from '@src/apis/server';
-import type { CategoryRes } from '@src/types/apis/category';
+import type { CategoryMoveReq, CategoryRes } from '@src/types/apis/category';
 import type { Category } from '@src/types/category';
-
+import { patchCategoryMove } from '@src/apis/category';
+import { getServerChannels } from '@src/apis/server';
 
 export const useCategory = () => {
   const serverId = useRecoilValue(currentServerIdState);
@@ -22,9 +22,50 @@ export const useCategory = () => {
         categoryId: it.categoryId,
         name: it.name,
       })) || [],
+    enabled: serverId !== -1,
   });
 
   return {
     categoryList,
   };
+};
+
+export const usePatchCategoryLocation = () => {
+  const queryClient = useQueryClient();
+  const serverId = useRecoilValue(currentServerIdState);
+
+  const editLocation = useMutation({
+    mutationFn: ({
+      categoryId,
+      body,
+    }: {
+      categoryId: number;
+      body: CategoryMoveReq;
+    }) => patchCategoryMove(categoryId, body),
+    onMutate: async () => {
+      await queryClient.cancelQueries({
+        queryKey: ['getServerChannels', serverId],
+      });
+
+      const prevData = queryClient.getQueryData([
+        'getServerChannels',
+        serverId,
+      ]);
+
+      return { prevData };
+    },
+    onError: (error, data, context) => {
+      queryClient.setQueryData(
+        ['getServerChannels', serverId],
+        context?.prevData,
+      );
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['getServerChannels', serverId],
+      });
+    },
+  });
+
+  return { editLocation };
 };
