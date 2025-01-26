68bd67ecd70d27842a2327df7c31f7776b80e131
yyeonzu <yyeonzuu@gmail.com>
2025-01-26 21:13:31 +0900

    :sparkles: 초대 코드 관련 react-query 함수 추가 (#274)

--- src/hooks/query/useServerTmp.ts
+++ src/hooks/query/useServerTmp.ts
@@ -1,10 +1,14 @@
 import { useNavigate } from 'react-router-dom';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 import type { PostServerReq } from '@src/types/apis/server';
 import useToast from '@src/hooks/useToast';
 import { encodeId } from '@src/utils/formatters';
 import { ROUTE_PATH } from '@src/constants/routePath';
-import { postServer } from '@src/apis/server';
+import {
+  getServerByCode,
+  postServer,
+  postServerJoinByCode,
+} from '@src/apis/server';
 
 /* 서버 생성 */
 export const usePostServer = (resetFields: () => void) => {
@@ -18,6 +22,33 @@ export const usePostServer = (resetFields: () => void) => {
       resetFields();
       const encodedId = encodeId(res.data.serverId);
       navigate(ROUTE_PATH.server.replace(':serverId', encodedId));
+    },
+  });
+  return mutation;
+};
+
+/* 초대 코드로 서버 확인 */
+export const useGetServerByCode = (inviteCode: string) => {
+  return useQuery({
+    queryKey: ['getServerByCode', inviteCode],
+    queryFn: () => getServerByCode(inviteCode),
+    select: (response) => response.data,
+  });
+};
+
+export const usePostServerJoin = (inviteCode: string) => {
+  const navigate = useNavigate();
+  const addToast = useToast();
+  const mutation = useMutation({
+    mutationFn: () => postServerJoinByCode(inviteCode),
+    onSuccess: (res) => {
+      addToast('success', '가입 완료');
+      const { serverId } = res.data;
+      const path = ROUTE_PATH.server.replace(
+        ':serverId',
+        encodeId(serverId || -1),
+      );
+      navigate(path);
     },
   });
   return mutation;
