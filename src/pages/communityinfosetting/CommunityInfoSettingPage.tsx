64575458e4c014ce1170a32b7ebe6b06c2b5f520
yyeonzu <yyeonzuu@gmail.com>
2025-01-26 23:24:00 +0900

    :recycle: InfoSetting 페이지 개선 (#274)

--- src/pages/communityinfosetting/CommunityInfoSettingPage.tsx
+++ src/pages/communityinfosetting/CommunityInfoSettingPage.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import CommunityInfoSection, {
-  CommunityInfoProps,
-} from '@src/components/communityinfosetting/CommunityInfoSection';
+import CommunityInfoSection from '@src/components/communityinfosetting/CommunityInfoSection';
 import styled from 'styled-components';
 import Header from '@src/components/common/Header';
 import CommunitySettingSection from '@src/components/communityinfosetting/CommunitySettingSection';
-import { useQuery } from '@tanstack/react-query';
-import { AxiosError } from 'axios';
-import { getServerOne } from '@src/apis/server';
-import type { Server } from '@src/types/server';
 import useLoaderData from '@src/hooks/useRoaderData';
+import { useGetServerOne } from '@src/hooks/query/useServerTmp';
 
 export interface CommunityInfoType {
   name: string;
@@ -23,45 +18,29 @@ export type CommunityRoleType = 'admin' | 'user';
 
 const CommunityInfoSettingPage = () => {
   const headerText = '공동체 정보 및 설정 보기';
-  // const { serverId: id } = useParams<{ serverId: string }>();
   const { id: serverId } = useLoaderData<{ id: number }>();
-  if (!serverId)
-    throw new Error('CommunityInfoSettingPage: serverId is not provided');
-  // const serverId = parseInt(id, 10);
-  const { data, error, isLoading } = useQuery<
-    Omit<Server, 'serverId'>,
-    AxiosError
-  >({
-    queryKey: ['getServerOne', serverId],
-    queryFn: () => getServerOne(serverId),
-    enabled: !!serverId,
-  });
+  const { data: server, isLoading } = useGetServerOne(serverId);
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (!server) {
+    return <div>Not Found</div>;
+  }
 
-  const mapServerToCommunityInfo = (
-    server: Omit<Server, 'serverId'>,
-  ): CommunityInfoProps => ({
+  const communityInfo = {
     name: server.name,
     memberInfo: `방장 ${server.ownerNickname} · 멤버 ${server.memberCount}명`,
     creationDate: server.createdAt,
     description: server.description,
     serverImg: server.serverImg || '',
-  });
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error || !data) {
-    return <div>Error: 데이터를 불러오지 못했습니다.</div>;
-  }
-
-  const communityInfo = mapServerToCommunityInfo(data);
+  };
 
   return (
     <>
       <Header text={headerText} headerType='back' />
       <Container>
         <CommunityInfoSection {...communityInfo} />
-        <CommunitySettingSection isOwner={false} />
+        <CommunitySettingSection isOwner={server.isOwner} />
       </Container>
     </>
   );
@@ -75,6 +54,5 @@ const Container = styled.div`
   gap: 0.94rem;
   padding: 0.91rem 1.25rem;
   width: 100vw;
-  width: 100svw;
   background-color: ${({ theme }) => theme.colors.neutral50};
 `;
