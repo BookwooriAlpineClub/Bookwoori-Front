39823605c3ab36bab273f4bfaeb3f7cdc6f8b5ba
Hyerin Sung <hyerinx@naver.com>
2025-01-30 01:49:11 +0900

    ♻️ SegmentedButton 컴포넌트 반영 (#252)

--- src/pages/book/RecordListPage.tsx
+++ src/pages/book/RecordListPage.tsx
@@ -4,17 +4,18 @@ import { useState } from 'react';
 import styled from 'styled-components';
 import { NoDataTextLayout } from '@src/styles/mixins';
 import Header from '@src/components/common/Header';
+import SegmentedButton from '@src/components/common/SegmentedButton';
 import Li from '@src/components/book/RecordListItem';
 
 const RecordListPage = () => {
   const [status, setStatus] = useState<Record['status']>('READING');
-  const radioConfigs: {
-    status: Record['status'];
-    text: '읽고 싶어요' | '읽고 있어요' | '다 읽었어요';
+  const configs: {
+    value: Record['status'];
+    label: '읽고 싶어요' | '읽고 있어요' | '다 읽었어요';
   }[] = [
-    { status: 'WISH', text: '읽고 싶어요' },
-    { status: 'READING', text: '읽고 있어요' },
-    { status: 'FINISHED', text: '다 읽었어요' },
+    { value: 'WISH', label: '읽고 싶어요' },
+    { value: 'READING', label: '읽고 있어요' },
+    { value: 'FINISHED', label: '다 읽었어요' },
   ];
 
   const { recordList: data } = useRecord({ status });
@@ -22,21 +23,11 @@ const RecordListPage = () => {
   return (
     <Container>
       <Header text='책 기록' headerType='back' />
-      <Fieldset name='status'>
-        {radioConfigs.map((item) => (
-          <Label key={item.status}>
-            {item.text}
-            <input
-              name='status'
-              type='radio'
-              value={item.status}
-              onChange={(e) => setStatus(e.target.value as Record['status'])}
-              defaultChecked={item.status === 'READING'}
-            />
-          </Label>
-        ))}
-        <Background status={status} />
-      </Fieldset>
+      <SegmentedButton<Record['status']>
+        config={configs}
+        onSegmentChange={setStatus}
+        defaultValue='READING'
+      />
       <main>
         {data.length !== 0 ? (
           <Ul>
@@ -60,65 +51,6 @@ const Container = styled(NoDataTextLayout)`
   main {
     overflow-y: auto;
   }
-`;
-const Fieldset = styled.fieldset`
-  position: relative;
-
-  display: flex;
-
-  width: 22.0625rem;
-  height: 2.8125rem;
-  margin: 0 auto;
-  flex-shrink: 0;
-
-  border: ${({ theme }) => theme.colors.neutral0} 0.25rem solid;
-  border-radius: 50%;
-  background-color: ${({ theme }) => theme.colors.neutral0};
-
-  overflow: hidden;
-`;
-const Label = styled.label`
-  position: relative;
-  z-index: 3;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  width: 33.3%;
-  height: 100%;
-
-  ${({ theme }) => theme.fonts.mountain}
-  color: ${({ theme }) => theme.colors.blue300};
-
-  &:has(input[type='radio']:checked) {
-    color: ${({ theme }) => theme.colors.neutral0};
-  }
-
-  transition: color 0.3s ease-out;
-`;
-const Background = styled.div<{ status: Record['status'] }>`
-  position: absolute;
-  left: ${({ status }) => {
-    switch (status) {
-      case 'WISH':
-        return '0';
-      case 'READING':
-        return '33.3%';
-      case 'FINISHED':
-        return '66.8%';
-      default:
-        return '33.3%';
-    }
-  }};
-  z-index: 1;
-
-  width: 33.3%;
-  height: 100%;
-
-  background-color: ${({ theme }) => theme.colors.blue500};
-
-  transition: left 0.3s ease-out;
 `;
 const Ul = styled.ul`
   display: flex;
