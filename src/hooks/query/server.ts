95369993f4f45ca60a03b770a75004d4b9500e07
Hyerin Sung <hyerinx@naver.com>
2025-01-30 02:14:49 +0900

    🔨 useGetServerList 훅 수정 (#252)

--- src/hooks/query/server.ts
+++ src/hooks/query/server.ts
@@ -4,9 +4,10 @@ import { useQuery } from '@tanstack/react-query';
 import { getServers } from '@src/apis/server';
 
 const useGetServerList = () => {
-  return useQuery<GetServersRes, AxiosError>({
+  return useQuery<GetServersRes, AxiosError, GetServersRes['servers']>({
     queryKey: ['getServers'],
     queryFn: () => getServers(),
+    select: (rawData) => rawData?.servers,
     initialData: { servers: [] },
   });
 };
