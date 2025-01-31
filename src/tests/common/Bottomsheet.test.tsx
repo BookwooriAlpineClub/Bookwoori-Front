75089bdb8219335824e8c47d7d2c8e05dda88077
Hyerin Sung <hyerinx@naver.com>
2025-01-31 15:17:26 +0900

    🚨 테스트 beforeEach에서 render 함수를 사용하지 않음 (#292)

--- src/tests/common/Bottomsheet.test.tsx
+++ src/tests/common/Bottomsheet.test.tsx
@@ -31,6 +31,8 @@ describe('Bottomsheet', () => {
     const modal = document.createElement('div');
     modal.id = 'modal';
     document.body.appendChild(modal);
+  });
+  test('openBottomsheet()로 열고, closeBottomsheet()로 닫아야 한다.', () => {
     // 웹앱 렌더
     render(
       <>
@@ -41,28 +43,43 @@ describe('Bottomsheet', () => {
     // 바텀시트 열기
     const openBtn = screen.getByTestId('bottomsheet-open');
     fireEvent.click(openBtn);
-  });
-  test('openBottomsheet()로 열고, closeBottomsheet()로 닫아야 한다.', () => {
+    // 테스트
     const bottomsheet = screen.getByLabelText('bottomsheet');
-
     expect(bottomsheet).toBeInTheDocument();
-
     const closeBtn = screen.getByTestId('bottomsheet-close');
     fireEvent.click(closeBtn);
-
     setTimeout(() => expect(bottomsheet).not.toBeInTheDocument(), 300);
   });
   test('openBottomsheet()로 전달한 요소를 렌더해야 한다.', () => {
+    // 웹앱 렌더
+    render(
+      <>
+        <App />
+        <Bottomsheet />
+      </>,
+    );
+    // 바텀시트 열기
+    const openBtn = screen.getByTestId('bottomsheet-open');
+    fireEvent.click(openBtn);
+    // 테스트
     const closeBtn = screen.getByTestId('bottomsheet-close');
-
     expect(closeBtn).toBeInTheDocument();
   });
   test('Scrim을 클릭하면 닫혀야 한다.', () => {
+    // 웹앱 렌더
+    render(
+      <>
+        <App />
+        <Bottomsheet />
+      </>,
+    );
+    // 바텀시트 열기
+    const openBtn = screen.getByTestId('bottomsheet-open');
+    fireEvent.click(openBtn);
+    // 테스트
     const bottomsheet = screen.getByLabelText('bottomsheet');
-
     const scrim = screen.getByLabelText('scrim');
     fireEvent.click(scrim);
-
     setTimeout(() => expect(bottomsheet).not.toBeInTheDocument(), 300);
   });
 });
