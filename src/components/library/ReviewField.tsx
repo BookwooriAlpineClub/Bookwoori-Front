06a59197917d160fef3335331b4e53dcb705cf54
Hyerin Sung <hyerinx@naver.com>
2025-02-04 19:01:35 +0900

    ♻️ ReviewField 컴포넌트 개선 (#194)

--- src/components/library/ReviewField.tsx
+++ src/components/library/ReviewField.tsx
@@ -1,41 +1,24 @@
-import { useRef, useState, useEffect } from 'react';
 import styled from 'styled-components';
+import TextField from '@src/components/common/input/TextField';
 import { ReactComponent as IcnStar } from '@src/assets/icons/md_star.svg';
 
 interface Props {
-  name: { num: string; str: string };
   readOnly?: boolean;
   num: number;
-  setNum?: React.Dispatch<React.SetStateAction<number>>;
+  setNum: React.Dispatch<React.SetStateAction<number>>;
   str: string;
-  setStr?: React.Dispatch<React.SetStateAction<string>>;
+  setStr: React.Dispatch<React.SetStateAction<string>>;
 }
 
-const ReviewField = ({
-  name,
-  readOnly = false,
-  num,
-  setNum,
-  str,
-  setStr,
-}: Props) => {
-  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
-  const [textareaHeight, setTextareaHeight] = useState<number>(0);
-
-  useEffect(() => {
-    if (textareaRef.current) {
-      setTextareaHeight(textareaRef.current.scrollHeight);
-    }
-  }, [textareaRef.current?.value]);
-
+const ReviewField = ({ readOnly, num, setNum, str, setStr }: Props) => {
   return (
     <>
-      <StarFieldset>
+      <Star>
         {Array.from({ length: 5 }, (_, index) => (
           <label key={index}>
             {index < num ? <IcnStarBlue /> : <IcnStarGray />}
             <input
-              name={name.num}
+              name='star'
               type='radio'
               value={index + 1}
               required
@@ -44,15 +27,15 @@ const ReviewField = ({
             />
           </label>
         ))}
-      </StarFieldset>
-      <Textarea
-        ref={textareaRef}
-        $height={textareaHeight}
-        name={name.str}
+      </Star>
+      <TextField
+        as='textarea'
+        name='content'
         placeholder='이 책은 어떠셨나요? 감상평을 적어주세요.'
-        readOnly={readOnly}
-        defaultValue={str}
-        onChange={(e) => setStr?.(e.target.value)}
+        maxLength={-1}
+        required
+        value={str}
+        setValue={setStr}
       />
     </>
   );
@@ -60,7 +43,7 @@ const ReviewField = ({
 
 export default ReviewField;
 
-const StarFieldset = styled.fieldset`
+const Star = styled.div`
   display: flex;
   flex-flow: row nowrap;
   justify-content: center;
@@ -71,28 +54,4 @@ const IcnStarBlue = styled(IcnStar).attrs({ width: 40, height: 40 })`
 `;
 const IcnStarGray = styled(IcnStar).attrs({ width: 40, height: 40 })`
   color: ${({ theme }) => theme.colors.neutral400};
-`;
-const Textarea = styled.textarea<{ $height: number }>`
-  width: 100%;
-  height: ${({ $height }) => $height}px;
-  padding: ${({ theme }) => `${theme.padding[12]} ${theme.padding[16]}`};
-
-  border-radius: ${({ theme }) => theme.rounded[16]};
-  background-color: ${({ theme }) => theme.colors.neutral0};
-
-  ${({ theme }) => theme.fonts.body}
-  color: ${({ theme }) => theme.colors.neutral950};
-
-  resize: none;
-
-  &::placeholder {
-    text-align: center;
-    align-content: center;
-
-    color: ${({ theme }) => theme.colors.neutral400};
-  }
-
-  &:read-only {
-    background-color: transparent;
-  }
 `;
