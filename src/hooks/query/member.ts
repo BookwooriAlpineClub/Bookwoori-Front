2e4d755f8cc651ad58bd1678a3cea176cf7b5b5c
yyeonzu <yyeonzuu@gmail.com>
2025-02-18 21:27:05 +0900

    :recycle: 필요없는 enabled 조건 삭제 (#345)

--- src/hooks/query/member.ts
+++ src/hooks/query/member.ts
@@ -13,7 +13,6 @@ export const useGetProfile = (userId: number | 'me') => {
   const { data } = useQuery<ProfileRes, AxiosError>({
     queryKey: ['getProfile', userId],
     queryFn: () => getProfile(userId),
-    enabled: userId !== -1,
   });
 
   return { profileData: data };
