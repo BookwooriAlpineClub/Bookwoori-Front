0a8323cad7317951cc3318959079d6def7b9c087
Hyerin Sung <hyerinx@naver.com>
2025-02-26 12:12:58 +0900

    ♻️ device, notification 분리 (#199)

---
+++ src/types/apis/device.ts
@@ -0,0 +1,22 @@
+import type Device from '@src/types/device';
+
+interface Success {
+  isSuccess: boolean;
+  code: number;
+  message: string;
+}
+/**
+ * 기기 등록 조회
+ */
+export interface GetDeviceRes extends Success {
+  result: Device;
+}
+/**
+ * 기기 등록 추가
+ */
+export type PostDeviceRes = Success;
+export type PostDeviceReq = Pick<Device, 'platform' | 'token'>;
+/**
+ * 기기 등록 삭제
+ */
+export type DeleteDeviceRes = Success;
