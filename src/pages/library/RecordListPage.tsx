b5b2b8715e6cb3c8713c8740b7c19d72c4d765a9
Hyerin Sung <hyerinx@naver.com>
2025-02-28 01:56:55 +0900

    💄 책 기록 목록 페이지 레이아웃 반영 (#349)

--- src/pages/library/RecordListPage.tsx
+++ src/pages/library/RecordListPage.tsx
@@ -52,18 +52,16 @@ const RecordListPage = () => {
 export default RecordListPage;
 
 const Container = styled.div`
-  gap: ${({ theme }) => theme.gap[16]};
+  display: flex;
+  flex-flow: column nowrap;
 
-  main {
-    overflow-y: auto;
-  }
+  gap: ${({ theme }) => theme.gap[16]};
 `;
 const Ul = styled.ul`
   display: flex;
   flex-flow: row wrap;
-  gap: ${({ theme }) => `${theme.gap[16]} ${theme.gap[12]}`};
+  justify-content: space-between;
+  row-gap: ${({ theme }) => theme.gap[16]};
 
   margin-bottom: 1.25rem;
-
-  overflow-y: scroll;
 `;
