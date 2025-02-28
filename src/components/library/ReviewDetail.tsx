7e0612662772596af939a36de1569450eb7f037b
hyerin <hyerinx@ewha.ac.kr>
2025-02-28 21:23:34 +0900

    🐛 책 평가 삭제 기능 API 연결

--- src/components/library/ReviewDetail.tsx
+++ src/components/library/ReviewDetail.tsx
@@ -1,7 +1,11 @@
 import type Review from '@src/types/review';
+import useModal from '@src/hooks/useModal';
+import { useDeleteReview } from '@src/hooks/query/review';
+import { dialogState } from '@src/states/atoms';
 import { formatDate } from '@src/utils/formatters';
 import styled from 'styled-components';
 import Section from '@src/components/common/Section';
+import DeleteConfirmDialog from '@src/components/common/modal/DeleteConfirmDialog';
 import StarReview from '@src/components/library/StarReview';
 import { ReactComponent as HiOutlinePencil } from '@src/assets/icons/hi_outline_pencil.svg';
 import { ReactComponent as FiTrash2 } from '@src/assets/icons/fi_trash_2.svg';
@@ -11,12 +15,25 @@ interface Props extends Review {
 }
 
 const ReviewDetail = ({
+  reviewId,
   star,
   content,
   createdAt,
   modifiedAt,
   openBottomsheet,
 }: Props) => {
+  const { openModal: openDialog, closeModal: closeDialog } =
+    useModal(dialogState);
+  const { mutate: deleteReview } = useDeleteReview(reviewId);
+  const handleDeleteClick = () => {
+    openDialog(
+      <DeleteConfirmDialog
+        onClickDelete={deleteReview}
+        closeDialog={closeDialog}
+      />,
+    );
+  };
+
   return (
     <Section>
       <Container>
@@ -26,7 +43,7 @@ const ReviewDetail = ({
             <Button type='button' onClick={openBottomsheet}>
               <HiOutlinePencil width={20} height={20} />
             </Button>
-            <Button type='button' onClick={openBottomsheet}>
+            <Button type='button' onClick={handleDeleteClick}>
               <FiTrash2 width={20} height={20} />
             </Button>
           </ButtonWrapper>
