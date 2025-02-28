332a2a8f3968015c82ed02b77bacd8deeb959eb0
rwaeng <rwaeng0421@gmail.com>
2025-02-28 12:13:27 +0900

    🐛  닉네임 일정 글자 수 초과시 줄임표 (#343)

--- src/components/climbing/ClimbingRope.tsx
+++ src/components/climbing/ClimbingRope.tsx
@@ -146,6 +146,12 @@ const Profile = styled.button`
   ${({ theme }) => theme.fonts.body};
 `;
 const Nickname = styled.span`
+  width: 90px;
+  height: 20px;
+  
   text-align: center;
   color: ${({ theme }) => theme.colors.neutral950};
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
 `;
