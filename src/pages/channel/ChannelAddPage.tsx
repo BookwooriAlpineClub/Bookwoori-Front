e7a5c615e4e19b4d3f287e70eebef1770b33dbb6
Hyerin Sung <hyerinx@naver.com>
2025-02-28 14:07:01 +0900

    🐛 모임 추가 페이지 레이아웃 반영

--- src/pages/channel/ChannelAddPage.tsx
+++ src/pages/channel/ChannelAddPage.tsx
@@ -11,11 +11,14 @@ import { formatDate, decodeIdParam } from '@src/utils/formatters';
 import styled from 'styled-components';
 import Header from '@src/components/common/Header';
 import Fieldset from '@src/components/common/Fieldset';
+import Section from '@src/components/common/Section';
 import Button from '@src/components/common/button/Button';
 import RadioField from '@src/components/common/input/RadioField';
 import Dropdown from '@src/components/common/input/Dropdown';
 import TextField from '@src/components/common/input/TextField';
-import Datepicker, { type Period } from '@src/components/common/input/Datepicker';
+import Datepicker, {
+  type Period,
+} from '@src/components/common/input/Datepicker';
 import SearchBottomsheet from '@src/components/channel/SearchBottomsheet';
 import { ReactComponent as IcnHash } from '@src/assets/icons/bi_hash.svg';
 import { ReactComponent as IcnVoice } from '@src/assets/icons/hi_outline_volume_up.svg';
@@ -33,12 +36,16 @@ const ChannelAddPage = () => {
   const [kind, setKind] = useState<string>(defaultKind);
   const [category, setCategory] = useState<string>('');
   const [name, setName] = useState<string>('');
-  const [book, setBook] = useState<Pick<Book, 'title' | 'isbn13'>>({ title: '', isbn13: '' });
+  const [book, setBook] = useState<Pick<Book, 'title' | 'isbn13'>>({
+    title: '',
+    isbn13: '',
+  });
   const [date, setDate] = useState<Period>({ start: '', end: '' });
   const [description, setDescription] = useState<string>('');
 
   const navigate = useEncodedNavigate();
-  const { openModal: openBottomsheet, closeModal: closeBottomsheet } = useModal(bottomsheetState);
+  const { openModal: openBottomsheet, closeModal: closeBottomsheet } =
+    useModal(bottomsheetState);
   const calcTomorrow = (): Date => {
     const day = new Date();
     day.setDate(day.getDate() + 1);
@@ -87,86 +94,108 @@ const ChannelAddPage = () => {
   };
 
   return (
-    <Container>
+    <>
       <Header text='모임 추가하기' headerType='back' />
-      <Main>
+      <main>
         <Form
           id='channel-add-form'
           className='scroll-area'
           onSubmit={handleFormSubmit}
         >
-          <RadioField
-            name='모임 유형'
-            options={[
-              { value: 'chat', Icon: IcnHash, text: '문자' },
-              { value: 'voice', Icon: IcnVoice, text: '전화' },
-              { value: 'climb', Icon: IcnRun, text: '등반' },
-            ]}
-            defaultValue={defaultKind}
-            required
-            setValue={setKind}
-          />
+          <Fieldset title='모임 유형'>
+            <Section>
+              <RadioField
+                name='모임 유형'
+                options={[
+                  { value: 'chat', Icon: IcnHash, text: '문자' },
+                  { value: 'voice', Icon: IcnVoice, text: '전화' },
+                  { value: 'climb', Icon: IcnRun, text: '등반' },
+                ]}
+                defaultValue={defaultKind}
+                required
+                setValue={setKind}
+              />
+            </Section>
+          </Fieldset>
           {(kind === 'chat' || kind === 'voice') && (
-            <Dropdown
-              name='모임 분류'
-              placeholder='분류 선택'
-              options={categoryList.map((item) => ({
-                id: item.categoryId,
-                text: item.name,
-              }))}
-              required
-              value={category}
-              setValue={setCategory}
-            />
+            <Fieldset title='모임 분류'>
+              <Section>
+                <Dropdown
+                  name='모임 분류'
+                  placeholder='분류 선택'
+                  options={categoryList.map((item) => ({
+                    id: item.categoryId,
+                    text: item.name,
+                  }))}
+                  required
+                  value={category}
+                  setValue={setCategory}
+                />
+              </Section>
+            </Fieldset>
           )}
           {!!kind && (
-            <TextField
-              as='input'
-              name='모임 이름'
-              placeholder='모임 이름을 입력하세요.'
-              maxLength={20}
-              required
-              value={name}
-              setValue={setName}
-            />
+            <Fieldset title='모임 이름'>
+              <Section>
+                <TextField
+                  as='input'
+                  name='모임 이름'
+                  placeholder='모임 이름을 입력하세요.'
+                  maxLength={20}
+                  required
+                  value={name}
+                  setValue={setName}
+                />
+              </Section>
+            </Fieldset>
           )}
           {kind === 'climb' && (
             <>
               <Fieldset title='등반할 책'>
-                <InputSearch
-                  name='등반할 책'
-                  placeholder='책을 선택하세요.'
-                  type='text'
-                  value={book.title}
-                  readOnly
-                  required
-                  onClick={() => {
-                    openBottomsheet(
-                      <SearchBottomsheet
-                        setValue={setBook}
-                        closeBottomsheet={closeBottomsheet}
-                      />,
-                    );
-                  }}
-                />
+                <Section>
+                  <InputSearch
+                    name='등반할 책'
+                    placeholder='책을 선택하세요.'
+                    type='text'
+                    value={book.title}
+                    readOnly
+                    required
+                    onClick={() => {
+                      openBottomsheet(
+                        <SearchBottomsheet
+                          setValue={setBook}
+                          closeBottomsheet={closeBottomsheet}
+                        />,
+                      );
+                    }}
+                  />
+                </Section>
               </Fieldset>
-              <Datepicker
-                type='period'
-                name='등반 기간'
-                min={formatDate(calcTomorrow(), '$1-$2-$3')}
-                required
-                value={date}
-                setValue={setDate}
-              />
-              <TextField
-                as='textarea'
-                name='등반 설명'
-                placeholder='사람들에게 등반에 대해 알려주세요.'
-                maxLength={150}
-                required
-                value={description}
-                setValue={setDescription}
-              />
+              <Fieldset title='등반 기간'>
+                <Section>
+                  <Datepicker
+                    type='period'
+                    name='등반 기간'
+                    min={formatDate(calcTomorrow(), '$1-$2-$3')}
+                    required
+                    value={date}
+                    setValue={setDate}
+                  />
+                </Section>
+              </Fieldset>
+              <Fieldset title='등반 설명'>
+                <Section>
+                  <TextField
+                    as='textarea'
+                    name='등반 설명'
+                    placeholder='사람들에게 등반에 대해 알려주세요.'
+                    maxLength={150}
+                    required
+                    value={description}
+                    setValue={setDescription}
+                  />
+                </Section>
+              </Fieldset>
             </>
           )}
         </Form>
@@ -177,37 +206,15 @@ const ChannelAddPage = () => {
         >
           추가하기
         </Button>
-      </Main>
-    </Container>
+      </main>
+    </>
   );
 };
 
 export default ChannelAddPage;
 
-const Container = styled.div`
-  display: flex;
-  flex-flow: column nowrap;
-
-  height: 100%;
-`;
-const Main = styled.main`
-  display: flex;
-  flex-flow: column nowrap;
-  gap: 0.91rem;
-
-  flex-grow: 1;
-  margin: 0.91rem 5% 2.56rem;
-
-  overflow-y: auto;
-`;
 const Form = styled.form`
-  display: flex;
-  flex-flow: column nowrap;
   gap: 1.25rem;
-
-  flex-grow: 1;
-
-  overflow-y: scroll;
 `;
 const InputSearch = styled.input`
   width: 100%;
